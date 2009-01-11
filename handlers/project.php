80c897651c2e8752a1fad5b05a2a064d8fc3cef6
Jack Polgar <jack.polgar@gmail.com>
2009-01-11 14:46:58 +0000

    Added some comments to the files so its easier to understand.

--- handlers/project.php
+++ handlers/project.php
@@ -10,11 +10,14 @@ $project = $db->fetcharray($db->query("SELECT * FROM ".DBPREFIX."projects WHERE 
 
 // Check what page to display
 if(!isset($uri->seg[1])) {
+	// Project Info page
 	include(template('project'));
 } elseif($uri->seg[1] == "roadmap") {
+	// Roadmap Page
 	$milestones = array();
 	$fetchmilestones = $db->query("SELECT * FROM ".DBPREFIX."milestones WHERE project=".$project['id']." ORDER BY due ASC");
 	while($info = $db->fetcharray($fetchmilestones)) {
+		// Get Ticket Info
 		$info['tickets']['open'] = $db->numrows($db->query("SELECT projectid,status FROM ".DBPREFIX."tickets WHERE status >= 1 AND milestoneid='".$info['id']."'"));
 		$info['tickets']['closed'] = $db->numrows($db->query("SELECT projectid,status FROM ".DBPREFIX."tickets WHERE status='0' AND milestoneid='".$info['id']."'"));
 		$info['tickets']['total'] = $db->numrows($db->query("SELECT projectid,status FROM ".DBPREFIX."tickets WHERE milestoneid='".$info['id']."'"));
@@ -25,6 +28,7 @@ if(!isset($uri->seg[1])) {
 	unset($fetchmilestones,$info);
 	include(template('roadmap'));
 } elseif($uri->seg[1] == "tickets") {
+	// Tickets Page
 	$milestone = $db->fetcharray($db->query("SELECT * FROM ".DBPREFIX."milestones WHERE milestone='".$uri->seg[2]."' AND project='".$project['id']."' LIMIT 1"));
 	$tickets = array();
 	if($uri->seg[3] == "open") {
@@ -32,10 +36,11 @@ if(!isset($uri->seg[1])) {
 	} elseif($uri->seg[3] == "closed") {
 		$status = "status='0'";
 	}
+	// Get Tickets
 	$fetchtickets = $db->query("SELECT * FROM ".DBPREFIX."tickets WHERE $status AND milestoneid='".$milestone['id']."' AND projectid='".$project['id']."' ORDER BY priority DESC");
 	while($info = $db->fetcharray($fetchtickets)) {
-		$info['component'] = $db->fetcharray($db->query("SELECT * FROM ".DBPREFIX."components WHERE id='".$info['componentid']."' LIMIT 1"));
-		$info['owner'] = $user->getinfo($info['ownerid']);
+		$info['component'] = $db->fetcharray($db->query("SELECT * FROM ".DBPREFIX."components WHERE id='".$info['componentid']."' LIMIT 1")); // Get Component info
+		$info['owner'] = $user->getinfo($info['ownerid']); // Get owner info
 		$tickets[] = $info;
 	}
 	unset($fetchtickets,$info);
