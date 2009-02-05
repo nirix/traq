b031acd16ed3562550a41ab2a851d8d2256b4554
Jack Polgar <jack.polgar@gmail.com>
2009-02-05 16:18:30 +0000

    Adding style.php containing the basic layout.

---
+++ templates/style.php
@@ -0,0 +1,210 @@
+<style type="text/css">
+body {
+	background: #fff;
+	color: #000;
+	margin: 10px;
+	padding: 0;
+}
+body, th, td {
+	font: normal 14px verdana,arial,'Bitstream Vera Sans',helvetica,sans-serif;
+}
+h1, h2, h3, h4 {
+	font-family: arial,verdana,'Bitstream Vera Sans',helvetica,sans-serif;
+	font-weight: bold;
+	letter-spacing: -0.018em;
+}
+h1 {
+	font-size: 19px;
+	margin: .15em 1em 0 0;
+}
+h2 {
+	font-size: 16px;
+}
+h3 {
+	font-size: 14px;
+}
+hr {
+	border: none;
+	border-top: 1px solid #ccb;
+	margin: 2em 0;
+}
+img {
+	border: none;
+}
+.underline {
+	text-decoration: underline;
+}
+
+#wrapper {
+	width: 1000px;
+	margin: 0 auto;
+}
+
+/* Link styles */
+a {
+	text-decoration: none;
+	color: #b00;
+	border-bottom: 1px solid #bbb;
+}
+a:hover {
+	
+}
+h1 a, h2 a, h3 a, h4 a, h5 a, h6 a {
+	color: inherit;
+}
+
+.errormessage {
+	border: 1px solid #C00;
+	color: #C00;
+	padding: 5px;
+}
+
+/* Forms */
+div.form {
+	width: 600px;
+	margin: 0 auto;
+}
+div.form fieldset {
+	margin-bottom: 5px;
+}
+
+
+input, textarea, select {
+	margin: 2px;
+}
+input, select {
+	vertical-align: middle;
+}
+input[type=button], input[type=submit], input[type=reset] {
+	background: #eee;
+	color: #222;
+	border: 1px outset #ccc;
+	padding: .1em .5em;
+}
+input[type=button]:hover, input[type=submit]:hover, input[type=reset]:hover {
+	background: #fff;
+}
+input[type=button][disabled], input[type=submit][disabled],
+input[type=reset][disabled] {
+	background: #f6f6f6;
+	border-style: solid;
+	color: #999;
+}
+input[type=text], input[type=password], input.textwidget, textarea {
+	border: 1px solid #d7d7d7;
+}
+input[type=text], input[type=password], input.textwidget {
+	padding: .25em .5em;
+}
+input[type=text]:focus, input[type=password]:focus, input.textwidget:focus, textarea:focus {
+	border: 1px solid #886;
+}
+fieldset {
+	border: 1px solid #d7d7d7;
+	padding: .5em;
+	margin: 0;
+}
+fieldset.iefix {
+	background: transparent;
+	border: none;
+	padding: 0;
+	margin: 0;
+}
+* html fieldset.iefix {
+	width: 98%;
+}
+fieldset.iefix p {
+	margin: 0;
+}
+legend {
+	color: #999;
+	padding: 0 .25em;
+	font-size: 90%;
+	font-weight: bold;
+}
+label.disabled {
+	color: #d7d7d7;
+}
+fieldset.error {
+	border: 1px solid #C00;
+	color: #C00;
+}
+fieldset.error input[type=text], fieldset.error input[type=password] {
+	border: 1px solid #C00;
+}
+
+#header #head {
+	font-size: 26px;
+	font-weight: bold;
+}
+#header #head a {
+	color: #000;
+	border: 0;
+}
+#header #head a:hover {
+	background-color: transparent;
+}
+
+#content {
+	padding: 10px 0px;
+}
+
+#footer {
+	border-top: 1px solid #000;
+	padding: 5px 0px;
+}
+
+/* Navigation */
+#metanav {
+	margin-top: 5px;
+}
+.nav ul {
+	font-size: 0px;
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	text-align: left;
+}
+.nav li {
+	border-right: 1px solid #d7d7d7;
+	display: inline;
+	padding: 0 .75em;
+	white-space: nowrap;
+	font-size: 10px;
+}
+.nav li.last {
+	border-right: none;
+}
+
+/* Main navigation bar */
+#mainnav {
+	background: #f7f7f7;
+	border: 1px solid #000;
+	font: normal 10px verdana,'Bitstream Vera Sans',helvetica,arial,sans-serif;
+	margin: .66em 0 .33em;
+	padding: .2em 0;
+}
+#mainnav ul {
+	font-size: 0px;
+}
+#mainnav li {
+	border-right: none;
+	padding: .25em 0;
+	font-size: 10px;
+}
+#mainnav a {
+	border-right: 1px solid #000;
+	border-bottom: none;
+	color: #000;
+	padding: 2px 20px;
+}
+#mainnav a:hover {
+	background-color: #ccc;
+}
+#mainnav .active a {
+	background: #000;
+	border-top: none;
+	color: #fff;
+	font-weight: bold;
+}
+</style>
