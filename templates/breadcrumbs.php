e6680b7a5c545b88a81fbe8fef13ce3f9a664c52
Jack Polgar <jack.polgar@gmail.com>
2009-02-06 02:53:30 +0000

    Would help if I added the breadcrumbs.php template.

---
+++ templates/breadcrumbs.php
@@ -0,0 +1,1 @@
+<div class="breadcrumbs"><small><a href="<?=$uri->anchor()?>">Projects</a><? foreach($breadcrumbs as $url => $label) { ?> » <a href="<?=$url?>"><?=$label?></a><? } ?></small></div><br>
