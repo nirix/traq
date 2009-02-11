eb7ec1e881d30e17458bc80d4379a2b201a2dca0
Jack Polgar <jack.polgar@gmail.com>
2009-02-11 07:11:45 +0000

    Removed the replace function on the "<" character when entering tickets and comments into the DB.

--- handlers/project.php
+++ handlers/project.php
@@ -106,7 +106,7 @@ if(!isset($uri->seg[1])) {
 															   0,
 															   ".$ticketid.",
 															   '".$db->escapestring($_POST['summary'])."',
-															   '".$db->escapestring(str_replace('<','&lt;',$_POST['body']))."',
+															   '".$db->escapestring($_POST['body'])."',
 															   ".$project['id'].",
 															   ".$db->escapestring($_POST['milestone']).",
 															   ".$db->escapestring($_POST['version']).",
@@ -191,7 +191,7 @@ if(!isset($uri->seg[1])) {
 	} elseif($_POST['action'] == "comment") {
 		if($user->loggedin) {
 			if(!empty($_POST['comment'])) {
-				$db->query("INSERT INTO ".DBPREFIX."ticketcomments VALUES(0,".$user->info->uid.",'".$db->escapestring(str_replace('<','&lt;',$_POST['comment']))."',".$ticket['id'].",".time().")");
+				$db->query("INSERT INTO ".DBPREFIX."ticketcomments VALUES(0,".$user->info->uid.",'".$db->escapestring($_POST['comment'])."',".$ticket['id'].",".time().")");
 			}
 			header("Location: ".$uri->anchor($project['slug'],'ticket',$ticket['tid']));
 		}
