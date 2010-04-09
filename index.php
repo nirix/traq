c378d1d4105f8f64868fca2180388cc24e1b426e
Jack Polgar <jack.polgar@gmail.com>
2010-04-09 10:27:38 +0000

    Forgot to commit the index.php file with the call to the 404 template...

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
 /**
  * Traq 2
- * Copyright (c) 2009 Jack Polgar
- * All Rights Reserved
+ * Copyright (C) 2009, 2010 Jack Polgar
+ *
+ * This file is part of Traq.
+ * 
+ * Traq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Traq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Traq. If not, see <http://www.gnu.org/licenses/>.
  *
  * $Id$
  */
@@ -14,7 +28,7 @@ define('TRAQPATH',str_replace(pathinfo(__FILE__,PATHINFO_BASENAME),'',__FILE__))
 require('inc/global.php');
 
 // Project listing
-if($uri->seg[0] == '')
+if(empty($uri->seg[0]))
 {
 	require('handlers/projectlist.php');
 }
@@ -28,9 +42,9 @@ elseif(is_project($uri->seg[0]))
 {
 	require('handlers/project.php');
 }
-// Something we're not sure of...
+// Something we're not sure of... load the 404 page...
 else
 {
-	error('Not Found','The page ('.$uri->geturi().') cannot be found.');
+	include(template('404'));
 }
 ?>
