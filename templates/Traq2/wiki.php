b11cb4cee07cde2eea36331dea8ca6818a55706f
Jack Polgar <jack.polgar@gmail.com>
2010-04-23 03:13:24 +0000

    Wiki page title added to <title>

--- templates/Traq2/wiki.php
+++ templates/Traq2/wiki.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title><?php echo settings('title')?> / <?php echo $project['name']?></title>
+		<title><?php echo settings('title')?> / <?php echo $project['name']?> / <?php echo $wiki['title']?></title>
 		<?php require(template('headerinc')); ?>
 	</head>
 	<body id="project_info">
