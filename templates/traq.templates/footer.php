c695f31c89cc726945e44d76a3a9a6293e92e739
Jack Polgar <jack.polgar@gmail.com>
2010-12-17 12:36:21 +1100

    Templates moved to their own directory, Traq2 theme renamed to Traq 2.0

---
+++ templates/traq.templates/footer.php
@@ -0,0 +1,10 @@
+
+		<div class="clear"></div>
+		</div>
+		<div id="foot">
+			<?php ($hook = FishHook::hook('template_footer')) ? eval($hook) : false; ?>
+			<span id="powered_by">
+				<?php echo l('poweredby')?>
+			</span>
+		</div>
+</div>
