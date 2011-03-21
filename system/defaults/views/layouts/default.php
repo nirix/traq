fe2ff836e7ee6d2bc304a725977341f8f3825583
Jack Polgar <jack.polgar@gmail.com>
2011-03-22 09:27:17 +1100

    More work on the layout

--- system/defaults/views/layouts/default.php
+++ system/defaults/views/layouts/default.php
@@ -12,20 +12,40 @@
 	<body>
 		<div id="wrapper">
 			<header id="header">
+				<div class="width">
 				<h1><?php echo HTML::link(baseurl(), settings('title'))?></h1>
-				<nav>
-					<ul>
-						<li><?php echo HTML::link('#', 'test')?></li>
-						<li><?php echo HTML::link('#', 'test')?></li>
-						<li><?php echo HTML::link('#', 'test')?></li>
-					</ul>
-				</nav>
+					<nav>
+						<ul id="meta_nav">
+							<?php if(LOGGEDIN) { ?>
+							<li><?php echo HTML::link(baseurl('usercp'), l('UserCP'))?></li>
+							<li><?php echo HTML::link(baseurl('logout'), l('Logout'))?></li>
+							<?php } else { ?>
+							<li><?php echo HTML::link(baseurl('login'), l('Login'))?></li>
+							<li><?php echo HTML::link(baseurl('register'), l('Register'))?></li>
+							<?php } ?>
+						</ul>
+						<ul>
+							<?php if(isset($traq->project['id'])) { ?>
+							<li><?php echo HTML::link(baseurl($traq->project['slug']), l('Project Info'))?></li>
+							<li><?php echo HTML::link(baseurl($traq->project['slug'],'timeline'), l('Timeline'))?></li>
+							<li><?php echo HTML::link(baseurl($traq->project['slug'],'roadmap'), l('Roadmap'))?></li>
+							<li><?php echo HTML::link(baseurl($traq->project['slug'],'tickets'), l('Tickets'))?></li>
+							<?php } else { ?>
+							<li><?php echo HTML::link(baseurl(), l('Projects'))?></li>
+							<?php } ?>
+						</ul>
+					</nav>
+				</div>
 			</header>
 			<section id="page">
-				<?php echo $output; ?>
+				<div class="width">
+					<?php echo $output; ?>
+				</div>
 			</section>
 			<footer>
+				<div class="width">
 				<?php _l('powered_by')?>
+				</div>
 			</footer>
 		</div>
 	</body>
--- system/defaults/views/projects/index.php
+++ system/defaults/views/projects/index.php
@@ -1,11 +1,11 @@
 <h2><?php _l('Projects')?></h2>
 
-<ul>
+<ul class="list projects_list">
 	<?php foreach($projects as $project) { ?>
 	<li>
-		<h3><?php echo $project['name']?></h3>
-		
-		<?php echo $project['info']?>
+		<h3><?php echo HTML::link(baseurl($project['slug']), $project['name'])?></h3>
+		<div class="quick_nav"><?php echo HTML::link(baseurl($project['slug'],'roadmap'), l('Roadmap'))?> | <?php echo HTML::link(baseurl($project['slug'],'timeline'), l('Timeline'))?> | <?php echo HTML::link(baseurl($project['slug'],'tickets'), l('Tickets'))?></div>
+		<div class="info"><?php echo formattext($project['info'])?></div>
 	</li>
 	<?php }?>
 </ul>
