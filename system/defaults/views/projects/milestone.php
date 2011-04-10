63dc065c0d926d6577e3c4370125971010b3597d
Jack Polgar <jack.polgar@gmail.com>
2011-04-11 08:47:51 +1000

    Fixing localization issue with the view milestone page.

--- system/defaults/views/projects/milestone.php
+++ system/defaults/views/projects/milestone.php
@@ -1,4 +1,4 @@
-<h2><?php _l('Milestone x', $milestone['milestone'])?></h2>
+<h2><?php _l('milestone_x', $milestone['milestone'])?></h2>
 <?php View::render('projects/_milestone_progress', array('milestone'=>$milestone))?>
 <div class="project-info">
 	<?php echo formattext($milestone['info'])?>
