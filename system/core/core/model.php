3f9f7c834a743c961429bf29dd674104be5f81c9
Jack Polgar <jack@polgar.id.au>
2011-06-19 20:44:11 +1000

    Done rewriting database model has_many and belongs_to relationships.

--- system/core/core/model.php
+++ system/core/core/model.php
@@ -11,15 +11,14 @@ class Model
 	public static $db;
 	protected static $_name;
 	protected static $_primary = 'id';
-	protected static $_has_one;
 	protected static $_has_many;
 	protected static $_belongs_to;
 	
 	private $keys = array();
 	
-	public function __construct($data = array())
+	public function __construct($data = null)
 	{
-		if (count($data) > 0) {
+		if ($data !== null) {
 			foreach ($data as $key => $val) {
 				$this->keys[] = $key;
 				$this->$key = $val;
@@ -27,13 +26,14 @@ class Model
 		}
 	}
 	
-	public static function find($find, $value = null, $depth = 2, $curdepth = 0)
+	public function save()
 	{
-		if ($depth == $curdepth) {
-			return null;
-		}
-		
-		$row = static::$db->select()->from(static::$_name);
+	
+	}
+	
+	public static function find($find, $value = null)
+	{
+		$row = Database::link()->select()->from(static::$_name);
 		
 		if ($value === null) {
 			$row->where("`" . static::$_primary . "` = '?'", $find);
@@ -43,6 +43,37 @@ class Model
 		
 		$data = $row->limit(1)->exec()->fetchAssoc();
 		
+		$data = static::_get_data($data);
+		
+		return new static($data);
+	}
+	
+	public static function fetchAll($args = array())
+	{
+		$rows = Database::link()->select()->from(static::$_name);
+		
+		if (isset($args['where'])) {
+			foreach ($args['where'] as $where) {
+				$rows->where($where[0], $where[1]);
+			}
+		}
+		
+		if (isset($args['order'])) {
+			$rows->orderby($args['order'][0], $args['order'][1]);
+		}
+		
+		$rows = $rows->exec()->fetchAll();
+		
+		$data = array();
+		foreach ($rows as $row) {
+			$data[] = static::_get_data($row);
+		}
+		
+		return $data;
+	}
+	
+	private static function _get_data($data)
+	{
 		// Has many
 		if (static::$_has_many !== null) {
 			foreach (static::$_has_many as $has_many) {
@@ -61,60 +92,40 @@ class Model
 						$has_many['column'] = static::$_primary;
 					}
 					// Get the data
-					$data[$has_many[0]] = static::$db->select()->from($has_many['table'])->where($has_many['foreign_key'] . " = '?'", $data[$has_many['column']])->exec()->fetchAll();
+					$data[$has_many[0]] = Database::link()->select()->from($has_many['table'])->where($has_many['foreign_key'] . " = '?'", $data[$has_many['column']])->exec()->fetchAll();
 				}
 				// Looks like we're using the simple way.
 				else {
-					$data[$has_many] = static::$db->select()->from($has_many)->where(substr(static::$_name, 0, -1) . "_id = '?'", $data[static::$_primary])->exec()->fetchAll();
+					$data[$has_many] = Database::link()->select()->from($has_many)->where(substr(static::$_name, 0, -1) . "_id = '?'", $data[static::$_primary])->exec()->fetchAll();
 				}
 			}
-		}
-		
-		// Has one
-		if (static::$_has_one !== null) {
-			
 		}
 		
 		// Belongs to
 		if (static::$_belongs_to !== null) {
-			
-		}
-		
-		return new static($data);
-	}
-	
-	public static function fetchAll($args = array())
-	{
-		if (!isset($args['depth'])) {
-			$args['depth'] = 2;
-			$args['curdepth'] = 0;
-		}
-		if ($args['depth'] == $args['curdepth']) {
-			return null;
-		}
-		
-		$rows = static::$db->select()->from(static::$_name);
-		
-		if (isset($args['where'])) {
-			foreach ($args['where'] as $where) {
-				$rows->where($where[0], $where[1]);
+			foreach (static::$_belongs_to as $belongs_to) {
+				// Check if we're doing it the verbose way.
+				if (is_array($belongs_to)) {
+					// Different table?
+					if (!isset($belongs_to['table'])) {
+						$belongs_to['table'] = $belongs_to[0] . 's';
+					}
+					// Different foreign key?
+					if (!isset($belongs_to['foreign_key'])) {
+						$belongs_to['foreign_key'] = 'id';
+					}
+					// Different column?
+					if (!isset($belongs_to['column'])) {
+						$belongs_to['column'] = $belongs_to[0] . '_id';
+					}
+					// Get the data
+					$data[$belongs_to[0]] = Database::link()->select()->from($belongs_to['table'])->where($belongs_to['foreign_key'] . " = '?'", $data[$belongs_to['column']])->limit(1)->exec()->fetchAssoc();
+				}
+				// Looks like we're using the simple way.
+				else {
+					$data[$belongs_to] = Database::link()->select()->from($belongs_to . 's')->where("id = '?'", $data[$belongs_to . '_id'])->limit(1)->exec()->fetchAssoc();
+				}
 			}
-		}
-		
-		if (isset($args['order'])) {
-			$rows->orderby($args['order'][0], $args['order'][1]);
-		}
-		
-		$rows = $rows->exec()->fetchAll();
-		
-		$data = array();
-		foreach ($rows as $row) {
-			// Belongs to
-			if (static::$_belongs_to !== null) {
-				
-			}
-			
-			$data[] = $row;
 		}
 		
 		return $data;
