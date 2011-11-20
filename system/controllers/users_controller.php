91160204f3e57a3d79863431c3751562be2f6e7e
Jack Polgar <jack@polgar.id.au>
2011-11-21 04:49:31 +1100

    Users controller started.

---
+++ system/controllers/users_controller.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Traq
+ * Copyright (C) 2009-2011 Jack Polgar
+ * 
+ * This file is part of Traq.
+ * 
+ * Traq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ * 
+ * Traq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Traq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class UsersController extends AppController
+{
+	public function action_login()
+	{
+		
+	}
+	
+	public function action_logout()
+	{
+		
+	}
+	
+	/**
+	 * Handles the register page and account creation.
+	 */
+	public function action_register()
+	{
+		$user = new User;
+		View::set('user', $user);
+		
+		if (Request::$method == 'post')
+		{
+			$data = array(
+				'username', Request::$post['username'],
+				'password', Request::$post['password'],
+				'email', Request::$post['email'],
+				'group_id' => 3
+			);
+			$user->set($data);
+			
+			if ($user->is_valid())
+			{
+				$user->save();
+				Request::redirect(Request::base('login'));
+			}
+		}
+	}
+}
