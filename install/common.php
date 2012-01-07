fa2f4e2a4daaa5e7f7a96a2d08fd2c87622f5c86
Jack Polgar <nrx@nirix.net>
2012-01-07 14:29:53 +1100

    Fix issues with stupid magic quotes and the installer.

--- install/common.php
+++ install/common.php
@@ -18,6 +18,26 @@
  * along with Traq. If not, see <http://www.gnu.org/licenses/>.
  */
 
+// Strip magic quotes from request data.
+if (function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc())
+{
+	$quotes_sybase = strtolower(ini_get('magic_quotes_sybase'));
+	$unescape_function = (empty($quotes_sybase) || $quotes_sybase === 'off') ? 'stripslashes($value)' : 'str_replace("\'\'","\'",$value)';
+	$stripslashes_deep = create_function('&$value, $fn', '
+		if (is_string($value)) {
+			$value = ' . $unescape_function . ';
+		} else if (is_array($value)) {
+			foreach ($value as &$v) $fn($v, $fn);
+		}
+	');
+	
+	// Unescape data
+	$stripslashes_deep($_POST, $stripslashes_deep);
+	$stripslashes_deep($_GET, $stripslashes_deep);
+	$stripslashes_deep($_COOKIE, $stripslashes_deep);
+	$stripslashes_deep($_REQUEST, $stripslashes_deep);
+} 
+
 /**
  * Checks if Traq is already installed.
  */
