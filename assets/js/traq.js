2c515ec729ed0cfd16f26979a1a920a4afb3a17f
Jack Polgar <nrx@nirix.net>
2012-01-20 23:03:37 +1100

    Any element with the "data-confirm" attribute brings up an confirm dialog with the value when clicked.

--- assets/js/traq.js
+++ assets/js/traq.js
@@ -1,3 +1,29 @@
+/*
+ * Traq
+ * Copyright (C) 2009-2012 Jack Polgar
+ * 
+ * This file is part of Traq.
+ * 
+ * Traq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ * 
+ * Traq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Traq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 $(document).ready(function(){
 	$('textarea.editor').likeaboss();
+	
+	$('[data-confirm]').each(function(){
+		var e = $(this);
+		e.click(function(){
+			return confirm(e.attr('data-confirm'));
+		});
+	});
 });
