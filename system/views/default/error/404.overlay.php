50329f72983dba82445a9bfd475ce4360cc17007
Jack Polgar <nrx@nirix.net>
2012-01-24 22:48:58 +1100

    Tweaked the 404 error page a little, also create an overlay for it.

---
+++ system/views/default/error/404.overlay.php
@@ -0,0 +1,4 @@
+<h3><?php echo l('error:404_title'); ?></h3>
+<div class="content" align="center">
+	<?php echo l('error:404_message', $request); ?>
+</div>
--- system/views/default/error/404.php
+++ system/views/default/error/404.php
@@ -1,7 +1,7 @@
 <div class="content">
-	<section id="error_404" class="push-7 span-10">
+	<section id="error_404" class="push-6 span-11">
 		<h2 id="page_title"><?php echo l('error:404_title'); ?></h2>
-		<div class="box">
+		<div class="box" align="center">
 			<?php echo l('error:404_message', $request); ?>
 		</div>
 	</section>
