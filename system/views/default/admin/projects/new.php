b26bec52680c28d4ec7ce4274f767ec695260619
Jack Polgar <nrx@nirix.net>
2012-01-28 15:47:07 +1100

    Added page title to the new project page.

--- system/views/default/admin/projects/new.php
+++ system/views/default/admin/projects/new.php
@@ -1,4 +1,5 @@
 <div class="content">
+	<h3><?php echo l('new_project'); ?></h3>
 	<form action="<?php echo Request::full_uri(); ?>" method="post">
 		<?php show_errors($proj->errors); ?>
 		<div class="tabular box">
