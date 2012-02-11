1f7f40883e5f307a19ac1d4e5d48419602170ebb
Jack Polgar <nrx@nirix.net>
2012-02-11 21:08:53 +1100

    Project Info page tweaks.

--- system/views/default/projects/view.php
+++ system/views/default/projects/view.php
@@ -1,4 +1,6 @@
 <div class="content">
 	<h2 id="page_title"><?php echo $project->name; ?></h2>
-	<?php echo format_text($project->info); ?>
+	<div class="project_info">
+		<?php echo format_text($project->info); ?>
+	</div>
 </div>
