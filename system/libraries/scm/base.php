b940c24e8c9734b051c9d92998799fb72fc16099
Jack Polgar <nrx@nirix.net>
2012-02-24 03:51:14 +1100

    Added the base SCM class, which will act as the repository browser API.

---
+++ system/libraries/scm/base.php
@@ -0,0 +1,93 @@
+<?php
+/*!
+ * Traq
+ * Copyright (C) 2009-2012 Traq.io
+ * 
+ * This file is part of Traq.
+ * 
+ * Traq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ * 
+ * Traq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Traq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SCM Base class.
+ * Copyright (C) Jack Polgar
+ *
+ * @author Jack P.
+ * @copyright (C) Jack P.
+ * @package Traq
+ * @package SCM
+ */
+class SCMBase
+{
+	/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	 *
+	 *  This is the base class for all SCM types.
+	 *  It is essentially the API for the repository browser,
+	 *  none of this class is finalised and anything can change.
+	 *
+	 *  Join in on the discussion on the forum to help build the best
+	 *  API for the repository browser.
+	 *
+	 *  You can also join the IRC channel and discuss the API, but
+	 *  please post all discussion to the forum topic about it.
+	 *
+	 *!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	 */
+
+
+	protected $info;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param object $info Repository model object.
+	 */
+	public function __construct(&$info)
+	{
+		$this->info = $info;
+	}
+
+	/**
+	 * Fetches the revision info.
+	 *
+	 * @param string $revision Revision identifier.
+	 * @param string $path Directory path for the revision.
+	 */
+	public function revision($revision, $path = null)
+	{
+		throw new Exception("Method " . get_class($this) . "::" . __FUNCTION__ . "() not implemented");
+	}
+
+	/**
+	 * Fetches all the revisions.
+	 *
+	 * @param string $path Directory path.
+	 *
+	 * @return array
+	 */
+	public function revisions($path = null)
+	{
+		throw new Exception("Method " . get_class($this) . "::" . __FUNCTION__ . "() not implemented");
+	}
+
+	/**
+	 * Lists the directory for the path and revision.
+	 *
+	 * @param string $path Directory path.
+	 * @param string $revision Revision identifier.
+	 */
+	public function list_dir($path, $revision = null)
+	{
+		throw new Exception("Method " . get_class($this) . "::" . __FUNCTION__ . "() not implemented");
+	}
+}
