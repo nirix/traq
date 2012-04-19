6004843b3fd6387bef1c73c4372728d4a7aaf2aa
Jack Polgar <nrx@nirix.net>
2012-04-20 08:25:59 +1000

    Fix issue with project settings form.

--- system/views/default/admin/projects/new.php
+++ system/views/default/admin/projects/new.php
@@ -3,7 +3,7 @@
 	<form action="<?php echo Request::full_uri(); ?>" method="post">
 		<?php show_errors($proj->errors); ?>
 		<div class="tabular box">
-			<?php View::render('projects/settings/_form'); ?>
+			<?php View::render('projectsettings/options/_form'); ?>
 		</div>
 		<div class="actions">
 			<input type="submit" value="<?php echo l('create'); ?>" />
