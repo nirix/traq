e0445d9c8b222423208d575e374842fa42c785ec
Jack Polgar <nrx@nirix.net>
2012-07-20 20:33:25 +1000

    Plain text changelog done.

--- system/views/traq.templates/changelog_plaintext.php
+++ system/views/traq.templates/changelog_plaintext.php
@@ -3,8 +3,8 @@ header("Content-type: text/plain");
 foreach ($milestones as $milestone) {
 	echo l('milestone_x', $milestone['milestone']) . PHP_EOL;
 	foreach ($milestone['changes'] as $change) {
-		echo $types[$change['type']]['bullet'] . $change['summary'] . PHP_EOL;
+		echo $types[$change['type']]['bullet'] . ' ' . $change['summary'] . PHP_EOL;
 	}
-	echo $milestone['changelog_plaintext'];
+	echo trim(strip_tags($milestone['changelog_plaintext'])) . PHP_EOL;
+	echo PHP_EOL;
 }
-echo PHP_EOL;
