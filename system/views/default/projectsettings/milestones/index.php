d5f22e21759588529abeb6dce0b71761763b7a4f
arturo182 <arturo182@tlen.pl>
2012-07-23 19:39:25 +0200

    Added due and status columns to milestones' settings.

--- system/views/default/projectsettings/milestones/index.php
+++ system/views/default/projectsettings/milestones/index.php
@@ -11,6 +11,8 @@
 			<tr>
 				<th class="fixed_name"><?php echo l('name'); ?></th>
 				<th class="codename"><?php echo l('codename'); ?></th>
+				<th class="due"><?php echo l('due'); ?></th>
+				<th class="status"><?php echo l('status'); ?></th>
 				<th class="actions"><?php echo l('actions'); ?></th>
 			</tr>
 		</thead>
@@ -19,6 +21,8 @@
 			<tr>
 				<td><?php echo HTML::link($milestone->name, "{$project->slug}/settings/milestones/{$milestone->id}/edit", array('data-overlay' => true)); ?></td>
 				<td><?php echo $milestone->codename; ?></td>
+				<td><?php echo $milestone->due > 0 ? time_from_now($milestone->due, true) : ''; ?></td>
+				<td><?php echo $milestone->status == 0 ? l('cancelled') : ($milestone->status == 1 ? l('active') : l('completed')); ?></td>
 				<td>
 					<?php echo HTML::link(l('edit'), "{$project->slug}/settings/milestones/{$milestone->id}/edit", array('class' => 'button_edit', 'data-overlay' => true)); ?>
 					<?php echo HTML::link(l('delete'), "{$project->slug}/settings/milestones/{$milestone->id}/delete", array('class' => 'button_delete', 'data-confirm' => l('confirm.delete_x', $milestone->name))); ?>
