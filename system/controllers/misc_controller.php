df6ea6dd400b5fb3b42a92dad57d4011a8f81928
Jack Polgar <nrx@nirix.net>
2012-07-25 07:46:35 +1000

    Autocomplete shouldn't popup if there are no options.

--- system/controllers/misc_controller.php
+++ system/controllers/misc_controller.php
@@ -79,14 +79,18 @@ class MiscController extends Controller
 		header("Content-type: application/json");
 
 		// Get the users, and loop over them
-		$users = User::select('username')->where('username', Request::$request['term'] . "%", 'LIKE')->exec()->fetch_all();
+		$users = User::select('username')->where('username', str_replace('*', '%', Request::$request['term']) . "%", 'LIKE')->exec()->fetch_all();
 		$options = array();
 		foreach ($users as $user) {
 			// Add the user to the optionls array
 			$options[] = $user->username;
 		}
 
-		// Output in javascript array format
-		echo '["' . implode('","', $options) . '"]';
+		// Make sure there are some options
+		if (count($options))
+		{
+			// Output in javascript array format
+			echo '["' . implode('","', $options) . '"]';
+		}
 	}
 }
