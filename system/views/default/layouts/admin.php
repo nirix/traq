0846fa5a3344dcebe9022a133a0f32d0ce9911e3
Jack Polgar <nrx@nirix.net>
2012-07-25 00:54:18 +1000

    Added jQuery UI.

--- system/views/default/layouts/admin.php
+++ system/views/default/layouts/admin.php
@@ -8,7 +8,8 @@
 		<!--[if lt IE 8]>
 		<?php echo HTML::css_link(Request::base() . 'css.php?css=ie'); ?>
 		<![endif]-->
-		<?php echo HTML::js_inc('//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'); ?>
+		<?php echo HTML::js_inc('//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js'); ?>
+		<?php echo HTML::js_inc('//ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js'); ?>
 		<?php echo HTML::js_inc(Request::base() . 'js.php?js=all'); ?>
 		<?php echo HTML::js_inc(Request::base('_js.js')); ?>
 	</head>
