68e36af74f236f62e34ca8dfbc90fb5546ab5859
Jack Polgar <nrx@nirix.net>
2012-08-01 06:24:54 +1000

    New hook: 'usercp/_nav'

--- system/views/default/usercp/_nav.php
+++ system/views/default/usercp/_nav.php
@@ -2,5 +2,6 @@
 	<ul>
 		<li<?php echo iif(active_nav('/usercp'), ' class="active"')?>><?php echo HTML::link(l('information'), "/usercp"); ?></li>
 		<li<?php echo iif(active_nav('/usercp/password'), ' class="active"')?>><?php echo HTML::link(l('password'), "/usercp/password"); ?></li>
+		<?php FishHook::run('template:usercp/_nav', array($user)); ?>
 	</ul>
 </nav>
