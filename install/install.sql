bfe605a6776bfe7a55ccb06e0a4db3c78724d5b0
Jack Polgar <nrx@nirix.net>
2012-08-02 06:10:06 +1000

    Fixed the priorities ID field.

--- install/install.sql
+++ install/install.sql
@@ -172,7 +172,7 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `traq_priorities`;
 
 CREATE TABLE `traq_priorities` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
