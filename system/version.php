bffe4a2b9e1bc55f2ada29e4d50b32e07cf0986e
Jack Polgar <nrx@nirix.net>
2012-10-10 16:20:37 +1100

    Update version number

--- system/version.php
+++ system/version.php
@@ -4,30 +4,30 @@
  * Copyright (C) 2009-2012 Jack Polgar
  *
  * This file is part of Traq.
- * 
+ *
  * Traq is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3 only.
- * 
+ *
  * Traq is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Traq. If not, see <http://www.gnu.org/licenses/>.
  */
 
 // Traq Version
-$traq_version = '2.3.9';
+$traq_version = '2.4.0';
 
 // Version code
 // 3.2.1 would be 30201
 // 3.2.11 would be 30211
-$traq_version_code = '20309';
+$traq_version_code = '20400';
 
 // Database Version
 $db_revision = 21;
 
 // Define the TRAQVER constant...
-define("TRAQVER",$traq_version);
+define("TRAQVER", $traq_version);
