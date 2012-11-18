dd5b1fb694a017a4271fbec9f174de96d87c05f0
Jack Polgar <nrx@nirix.net>
2012-11-18 23:07:57 +1100

    CSS and JavaScript PHP files updated to use new code formatting rules.

--- css.php
+++ css.php
@@ -1,19 +1,19 @@
 <?php
 /*
  * Traq
- * Copyright (C) 2009-2012 Jack Polgar
- * 
+ * Copyright (C) 2009-2012 Traq.io
+ *
  * This file is part of Traq.
- * 
+ *
  * Traq is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3 only.
- * 
+ *
  * Traq is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Traq. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,17 +22,15 @@
 header("content-type: text/css; charset: UTF-8;");
 
 // Check if we can gzip the page or not/
-if (extension_loaded('zlib'))
-{
-	// We can!
-	ob_end_clean();
-	ob_start('ob_gzhandler');
+if (extension_loaded('zlib')) {
+    // We can!
+    ob_end_clean();
+    ob_start('ob_gzhandler');
 }
 
 // Check for the CSS index in the request array..
-if (!isset($_REQUEST['css']) and !isset($_REQUEST['theme']))
-{
-	exit;
+if (!isset($_REQUEST['css']) and !isset($_REQUEST['theme'])) {
+    exit;
 }
 
 // Fetch the request class.
@@ -43,25 +41,25 @@ $output = array();
 
 // assets/css files
 if (isset($_REQUEST['css'])) {
-	foreach (explode(',', $_REQUEST['css']) as $file) {
-		// Check if the file exists...
-		if (file_exists(__DIR__ . "/assets/css/{$file}.css")) {
-			// Add it to the output array.
-			$output[] = file_get_contents(__DIR__ . "/assets/css/{$file}.css");
-		}
-	}
+    foreach (explode(',', $_REQUEST['css']) as $file) {
+        // Check if the file exists...
+        if (file_exists(__DIR__ . "/assets/css/{$file}.css")) {
+            // Add it to the output array.
+            $output[] = file_get_contents(__DIR__ . "/assets/css/{$file}.css");
+        }
+    }
 }
 
 // Theme css files
 if (isset($_REQUEST['theme'])) {
-	$theme = htmlspecialchars($_REQUEST['theme']);
-	foreach (explode(',', $_REQUEST['theme']) as $file) {
-		// Check if the file exists...
-		if (file_exists(__DIR__ . "/vendor/traq/views/{$theme}/css/{$file}.css")) {
-			// Add it to the output array.
-			$output[] = file_get_contents(__DIR__ . "/vendor/traq/views/{$theme}/css/{$file}.css");
-		}
-	}
+    $theme = htmlspecialchars($_REQUEST['theme']);
+    foreach (explode(',', $_REQUEST['theme']) as $file) {
+        // Check if the file exists...
+        if (file_exists(__DIR__ . "/vendor/traq/views/{$theme}/css/{$file}.css")) {
+            // Add it to the output array.
+            $output[] = file_get_contents(__DIR__ . "/vendor/traq/views/{$theme}/css/{$file}.css");
+        }
+    }
 }
 
 $output = implode('', $output);
