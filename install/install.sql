dbe7aa5c45d2c937211be65b930c8f915f97b35c
Jack Polgar <nrx@nirix.net>
2013-03-21 20:00:17 +1100

    Add default value for project display order.

--- install/install.sql
+++ install/install.sql
@@ -235,7 +235,7 @@ CREATE TABLE `traq_projects` (
   `info` longtext COLLATE utf8_unicode_ci NOT NULL,
   `next_tid` bigint(20) NOT NULL DEFAULT '1',
   `enable_wiki` tinyint(1) NOT NULL DEFAULT '0',
-  `displayorder` bigint(20) NOT NULL,
+  `displayorder` bigint(20) NOT NULL DEFAULT '0',
   `private_key` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
