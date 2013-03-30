7c9813660294e8f9a641bcab84c54ab3e898a8c0
Jack Polgar <nrx@nirix.net>
2013-03-30 23:25:03 +1100

    Added ticket move events to timeline atom feed.

--- vendor/traq/views/default/projects/timeline.atom.php
+++ vendor/traq/views/default/projects/timeline.atom.php
@@ -9,12 +9,14 @@ foreach ($days as $day) {
         // Ticket created, closed and reopened
         if (in_array($row->action, array('ticket_created','ticket_closed','ticket_reopened'))) {
             $entry['title'] = l("timeline.{$row->action}",
-                $row->ticket()->summary,
-                $row->ticket()->ticket_id,
-                $row->ticket()->type->name,
-                $row->ticket_status()->name
+                array(
+                    'ticket_summary'     => $row->ticket()->summary,
+                    'ticket_id'          => $row->ticket()->ticket_id,
+                    'ticket_type_name'   => $row->ticket()->type->name,
+                    'ticket_status_name' => $row->ticket_status()->name
+                )
             );
-            $entry['id'] = "timeline:{$row->id}:ticket:{$row->ticket()->ticket_id}";
+            $entry['id'] = "timeline:{$row->id}:ticket:{$row->ticket()->ticket_id}:{$row->action}";
             $entry['author'] = array(
                 'name' => $row->user->name
             );
@@ -29,7 +31,13 @@ foreach ($days as $day) {
         // Ticket comments
         elseif ($row->action == 'ticket_comment') {
             $entry['title'] = l('timeline.ticket_comment', $row->ticket()->summary, $row->ticket()->ticket_id);
-            $entry['id'] = "timeline:{$row->id}:ticket:{$row->ticket()->ticket_id}";
+            $entry['id'] = "timeline:{$row->id}:ticket:{$row->ticket()->ticket_id}:commented";
+            $entry['link'] = "http://" . $_SERVER['HTTP_HOST'] . Request::base($row->ticket()->href());
+        }
+        // Moved tickets
+        elseif ($row->action == 'ticket_moved_from' or $row->action == 'ticket_moved_to') {
+            $entry['title'] = l("timeline.{$row->action}", array('ticket' => $row->ticket()->summary, 'project' => $row->other_project()->name));
+            $entry['id'] = "timeline:{$row->id}:ticket:{$row->ticket()->ticket_id}:moved";
             $entry['link'] = "http://" . $_SERVER['HTTP_HOST'] . Request::base($row->ticket()->href());
         }
 
