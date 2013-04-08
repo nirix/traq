3552543cdacbdf19913ee07b0af65028f73926d7
Jack Polgar <nrx@nirix.net>
2013-04-08 13:25:04 +1000

    Removed the "Set all ticket properties" permission.

--- vendor/traq/locale/enus.php
+++ vendor/traq/locale/enus.php
@@ -290,7 +290,6 @@ class enUS extends \traq\libraries\Locale
                     'vote_on_tickets'           => "Vote",
                     'comment_on_tickets'        => "Comment",
                     'edit_ticket_description'   => "Edit description",
-                    'set_all_ticket_properties' => "Set all properties",
                     'add_attachments'           => "Add attachments",
                     'view_attachments'          => "View attachments",
                     'delete_attachments'        => "Delete attachments",
