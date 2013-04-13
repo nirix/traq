3749bc1d4af9d8217d430e612cd47dac4935d994
Jack Polgar <nrx@nirix.net>
2013-04-13 13:12:55 +1000

    Started the JavaScript for the mass actions.

---
+++ assets/js/mass_actions.js
@@ -0,0 +1,33 @@
+/*!
+ * Traq
+ * Copyright (C) 2009-2013 Traq.io
+ * Copyright (C) 2009-2013 J. Polgar
+ * https://github.com/nirix
+ *
+ * This file is part of Traq.
+ *
+ * Traq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ *
+ * Traq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Traq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$(document).ready(function(){
+	var selected_tickets = [];
+
+	// Loop over checkboxes
+	$('#tickets input[type="checkbox"]').each(function(){
+		// Add click event
+		$(this).on('click', function(){
+			// Add ticket ID to selected tickets
+			selected_tickets.push($(this).val());
+		});
+	});
+});
