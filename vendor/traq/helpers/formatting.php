34b5dc15eca78199b083a8553823ce6ba8896d3f
Jack Polgar <nrx@nirix.net>
2013-05-04 13:13:23 +1000

    Tickets can now be linked to with `#123` and `project#123`.

--- vendor/traq/helpers/formatting.php
+++ vendor/traq/helpers/formatting.php
@@ -35,5 +35,42 @@ function format_text($text, $strip_html = true)
 
     FishHook::run('function:format_text', array(&$text, $strip_html));
 
+    // Ticket links
+    $text = ticket_links($text);
+
+    return $text;
+}
+
+/**
+ * Links #123 and project#123 to the corresponding ticket.
+ *
+ * @param string $text
+ *
+ * @return string
+ */
+function ticket_links($text)
+{
+    $match = preg_match_all("/\s+(?P<ticket_id>(?:[a-zA-Z0-9\-\_]+)?#[0-9]+)\s+/", $text, $matches);
+    foreach ($matches['ticket_id'] as $match) {
+        $match = explode('#', $match);
+
+        // Replace project#123
+        if ($project = Project::find('slug', $match[0])) {
+            $text = str_replace(
+                "{$project->slug}#{$match[1]}",
+                HTML::link("{$project->slug}#{$match[1]}", $project->href("tickets/{$match[1]}")),
+                $text
+            );
+        }
+        // Replace #123
+        elseif (empty($match[0])) {
+            $text = str_replace(
+                " #{$match[1]} ",
+                " " . HTML::link("#{$match[1]}", Avalon::app()->project->href("tickets/{$match[1]}")) . " ",
+                $text
+            );
+        }
+    }
+
     return $text;
 }
