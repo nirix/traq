23bfcf0b84a080f105991d2bd66f70606035436f
Jack Polgar <jack@polgar.id.au>
2013-09-06 00:27:08 +1000

    Version 3.3

--- vendor/traq/version.php
+++ vendor/traq/version.php
@@ -19,14 +19,14 @@
  */
 
 // Version
-define("TRAQ_VER", "3.2.2"); // Pretty obvious...
+define("TRAQ_VER", "3.3"); // Pretty obvious...
 
 // Version code
 // 1.2.3   -> 10203
 // 1.3(.0) -> 10300
 // 1.12.1  -> 11201
 // 1.2.11  -> 10211
-define("TRAQ_VER_CODE", 30202); // Used to check for new versions
+define("TRAQ_VER_CODE", 30300); // Used to check for new versions
 define("TRAQ_DB_VER", 30202);   // Database version
 
 // API version
