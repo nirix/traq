718aa3bd5176437af7804151fc27e2a087b6dd95
Jack Polgar <jack@polgar.id.au>
2014-06-12 06:08:58 +1000

    Update version to 4.0.0-dev

--- vendor/Traq/version.php
+++ vendor/Traq/version.php
@@ -22,15 +22,15 @@
  */
 
 // Version
-define("TRAQ_VER", "3.5.1"); // Pretty obvious...
+define("TRAQ_VER", "4.0.0-dev"); // Pretty obvious...
 
 // Version code
 // 1.2.3   -> 10203
 // 1.3(.0) -> 10300
 // 1.12.1  -> 11201
 // 1.2.11  -> 10211
-define("TRAQ_VER_CODE", 30501); // Used to check for new versions
-define("TRAQ_DB_VER", 30500);   // Database version
+define("TRAQ_VER_CODE", 40000); // Used to check for new versions
+define("TRAQ_DB_VER",   30500); // Database version
 
 // API version
-define("TRAQ_API_VER", "1.0");
+define("TRAQ_API_VER", "2.0");
