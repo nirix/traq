a01e1c7edf88b455ef2a9dd914b0e4dc5c86e98a
Jack Polgar <jack@polgar.id.au>
2014-07-04 23:55:26 +1000

    Updated `UserRole` model.

--- vendor/Traq/Models/UserRole.php
+++ vendor/Traq/Models/UserRole.php
@@ -1,7 +1,10 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2012 Traq.io
+ * Copyright (C) 2009-2014 Jack Polgar
+ * Copyright (C) 2012-2014 Traq.io
+ * https://github.com/nirix
+ * http://traq.io
  *
  * This file is part of Traq.
  *
@@ -18,33 +21,27 @@
  * along with Traq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace traq\models;
+namespace Traq\Models;
 
-use avalon\database\Model;
+use Radium\Database\Model;
 
 /**
  * Users<>Roles model.
  *
- * @package Traq
- * @subpackage Models
+ * @package Traq\Models
  * @author Jack P.
  * @copyright (c) Jack P.
  */
 class UserRole extends Model
 {
-    protected static $_name = 'user_roles';
-    protected static $_properties = array(
-        'id',
-        'user_id',
-        'project_id',
-        'project_role_id'
-    );
-
     // Allow easy access to the project and role models
-    protected static $_belongs_to = array(
+    protected static $_belongsTo = array(
         'project', 'user',
 
-        'role' => array('model' => 'ProjectRole', 'column' => 'project_role_id')
+        'role' => array(
+            'model' => 'ProjectRole',
+            'column' => 'project_role_id'
+        )
     );
 
     /**
@@ -52,28 +49,23 @@ class UserRole extends Model
      *
      * @return array
      */
-    public static function project_members($project_id)
+    public static function projectMembers($project_id)
     {
         $members = array();
 
         // Loop over the relations and add the user to the array
-        foreach (static::select()->where('project_id', $project_id)->exec()->fetch_all() as $relation) {
+        foreach (static::select()->where('project_id = ?', $project_id)->fetchAll() as $relation) {
             $members[] = $relation->user;
         }
 
         return $members;
     }
 
-    public function is_valid()
-    {
-        return true;
-    }
-
     public function __toArray($fields = null)
     {
         $data = parent::__toArray($fields);
-        $data['user'] = $this->user->__toArray();
-        $data['role'] = $this->role->__toArray();
+        $data['user'] = $this->user()->__toArray();
+        $data['role'] = $this->role()->__toArray();
         return $data;
     }
 }
