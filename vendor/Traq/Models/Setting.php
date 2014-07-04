168e4103ade3153ff82d6b4bfc6e752e949f8af8
Jack Polgar <jack@polgar.id.au>
2014-07-04 23:54:32 +1000

    Settings model updated.

--- vendor/Traq/Models/Setting.php
+++ vendor/Traq/Models/Setting.php
@@ -1,7 +1,10 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2012 Traq.io
+ * Copyright (C) 2009-2014 Jack Polgar
+ * Copyright (C) 2012-2014 Traq.io
+ * https://github.com/nirix
+ * http://traq.io
  *
  * This file is part of Traq.
  *
@@ -18,30 +21,23 @@
  * along with Traq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace traq\models;
+namespace Traq\Models;
 
-use avalon\database\Model;
+use Radium\Database\Model;
 
 /**
  * Setting model.
  *
- * @package Traq
- * @subpackage Models
+ * @package Traq\Models
  * @since 3.0
  * @author Jack P.
  * @copyright (c) Jack P.
  */
 class Setting extends Model
 {
-    protected static $_name = 'settings';
-    protected static $_primary_key = 'setting';
-    protected static $_properties = array(
-        'setting',
-        'value'
+    protected static $_primaryKey = 'setting';
+
+    protected static $_validates = array(
+        'setting' => array('required')
     );
-    
-    public function is_valid()
-    {
-        return !empty($this->_data['setting']);
-    }
 }
