c7c146294957fcf8f66ce2f72c0b5bcc83b9d70b
Jack Polgar <jack@polgar.id.au>
2014-07-05 12:04:18 +1000

    Updated ticket status model.

---
+++ vendor/Traq/Models/Status.php
@@ -0,0 +1,73 @@
+<?php
+/*!
+ * Traq
+ * Copyright (C) 2009-2014 Jack Polgar
+ * Copyright (C) 2012-2014 Traq.io
+ * https://github.com/nirix
+ * http://traq.io
+ *
+ * This file is part of Traq.
+ *
+ * Traq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ *
+ * Traq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Traq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Traq\Models;
+
+use Radium\Database\Model;
+use Radium\Language;
+
+/**
+ * Status model.
+ *
+ * @package Traq\Models
+ * @since 3.0
+ * @author Jack P.
+ * @copyright (c) Jack P.
+ */
+class Status extends Model
+{
+    protected static $_validates = array(
+        'name' => array('required', 'unique')
+    );
+
+    /**
+     * Returns an array formatted for the Form::select() method.
+     *
+     * @return array
+     */
+    public static function selectOptions()
+    {
+        $open   = Language::translate('open');
+        $closed = Language::translate('closed');
+
+        $options = array(
+            $open   => array(),
+            $closed => array()
+        );
+
+        foreach (static::all() as $status) {
+            $option = array(
+                'label' => $status->name,
+                'value' => $status->id
+            );
+
+            if ($status->status) {
+                $options[$open][] = $option;
+            } else {
+                $options[$closed][] = $option;
+            }
+        }
+
+        return $options;
+    }
+}
