c3a873bd07154467653ec0fecb161220a6ac1c2f
Jack Polgar <jack@polgar.id.au>
2014-07-08 00:59:14 +1000

    Update plugin model.

--- vendor/Traq/Models/Plugin.php
+++ vendor/Traq/Models/Plugin.php
@@ -1,7 +1,10 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2012 Traq.io
+ * Copyright (C) 2009-2014 Jack Polgar
+ * Copyright (C) 2012-2014 Traq.io
+ * https://github.com/nirix
+ * http://traq.io
  *
  * This file is part of Traq.
  *
@@ -18,9 +21,9 @@
  * along with Traq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace traq\models;
+namespace Traq\Models;
 
-use avalon\database\Model;
+use Radium\Database\Model;
 
 /**
  * Plugin model.
@@ -32,19 +35,7 @@ use avalon\database\Model;
  */
 class Plugin extends Model
 {
-    protected static $_name = 'plugins';
-    protected static $_properties = array(
-        'id',
-        'file',
-        'enabled'
+    protected static $validates = array(
+        'directory' => array('required', 'unique')
     );
-
-    public function is_valid()
-    {
-        // Make sure the file field isnt blank
-        if (empty($this->_data['file'])) {
-            return false;
-        }
-        return true;
-    }
 }
