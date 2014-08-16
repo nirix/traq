0bfc281c84311b5803835030680ad4efa171c737
Jack Polgar <jack@polgar.id.au>
2014-08-16 16:28:31 +1000

    Alias the errors helper.

--- vendor/Traq/Traq.php
+++ vendor/Traq/Traq.php
@@ -78,6 +78,7 @@ class Traq extends Application
         class_alias("Traq\Helpers\Format", "Format");
         class_alias("Traq\Helpers\Subscription", "Subscription");
         class_alias("Traq\Helpers\TWBS", "TWBS");
+        class_alias("Traq\Helpers\Errors", "Errors");
     }
 
     protected function loadPlugins()
