e586e981de4dd7621e84d6c7eceac7eb2dfd2067
Jack Polgar <jack@polgar.id.au>
2014-10-19 23:33:09 +1100

    Reorganise views directory.

--- vendor/traq/traq/views/_misc/preview_text.php
+++
@@ -1,9 +0,0 @@
-<h3><?php echo l('preview'); ?></h3>
-<div class="content">
-	<?php echo $data?>
-</div>
-<form>
-	<div class="actions">
-		<input type="button" value="<?php echo l('close'); ?>" onclick="close_overlay();" />
-	</div>
-</form>
