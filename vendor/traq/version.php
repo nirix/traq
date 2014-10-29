8ff1dbe7f1b59f593286f90b7a5324137b5facf1
Jack Polgar <jack@polgar.id.au>
2014-10-30 07:55:43 +1100

    Bump version number to 3.5.2

--- vendor/traq/version.php
+++ vendor/traq/version.php
@@ -22,14 +22,14 @@
  */
 
 // Version
-define("TRAQ_VER", "3.5.1"); // Pretty obvious...
+define("TRAQ_VER", "3.5.2"); // Pretty obvious...
 
 // Version code
 // 1.2.3   -> 10203
 // 1.3(.0) -> 10300
 // 1.12.1  -> 11201
 // 1.2.11  -> 10211
-define("TRAQ_VER_CODE", 30501); // Used to check for new versions
+define("TRAQ_VER_CODE", 30502); // Used to check for new versions
 define("TRAQ_DB_VER", 30500);   // Database version
 
 // API version
