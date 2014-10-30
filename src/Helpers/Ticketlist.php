dee71598d949c6aa0978373fadf8b4646c6617c2
Jack Polgar <jack@polgar.id.au>
2014-10-31 07:36:15 +1100

    Move more ticket listing helpers to the new helper.

--- src/Helpers/Ticketlist.php
+++ src/Helpers/Ticketlist.php
@@ -25,6 +25,7 @@ namespace Traq\Helpers;
 
 use Radium\Language;
 use Radium\Http\Request;
+use Radium\Helpers\Time;
 
 /**
  * Ticket listing helper
@@ -35,6 +36,25 @@ use Radium\Http\Request;
  */
 class Ticketlist
 {
+    /**
+     * Default ticket columns.
+     *
+     * @return array
+     */
+    public static function defaultColumns()
+    {
+        return [
+            'ticket_id',
+            'summary',
+            'status',
+            'owner',
+            'type',
+            'component',
+            'milestone',
+            'created_at'
+        ];
+    }
+
     /**
      * Returns the table columns allowed on the listing page.
      *
@@ -141,5 +161,169 @@ class Ticketlist
         }
 
         return [$property, strtolower($order[1]) == 'asc' ? "ASC" : "DESC"];
+    }
+
+    /**
+     * Returns the URL for sorting the provided ticket column.
+     *
+     * @param string $column
+     *
+     * @return string
+     */
+    public static function sortUrlFor($column)
+    {
+        // Get current order
+        if (isset(Request::$request['order_by'])) {
+            $order = explode('.', Request::$request['order_by']);
+        } else {
+            return Request::requestUri() . (strlen($_SERVER['QUERY_STRING']) ? '&amp;' : '?') . "order_by={$column}.asc";
+        }
+
+        // Are we flipping the current sort?
+        if ($order[0] == $column) {
+            $query = "{$column}." . (strtolower($order[1]) == 'asc' ? 'desc' : 'asc');
+        } else {
+            $query = "{$column}.{$order[1]}";
+        }
+
+        return str_replace("order_by=". implode('.', $order), "order_by={$query}", Request::requestUri());
+    }
+
+    /**
+     * Returns the content for the ticket listing headers.
+     *
+     * @param string $column The column to get the content for.
+     *
+     * @return mixed
+     */
+    public static function headerFor($column)
+    {
+        switch ($column) {
+            case 'ticket_id':
+                return Language::translate('id');
+                break;
+
+            case 'summary':
+            case 'status':
+            case 'owner':
+            case 'type':
+            case 'component':
+            case 'milestone':
+            case 'assigned_to':
+            case 'updates':
+            case 'votes':
+            case 'priority':
+            case 'severity':
+                return Language::translate($column);
+
+            case 'created_at':
+                return Language::translate('created');
+                break;
+
+            case 'updated_at':
+                return Language::translate('updated');
+                break;
+        }
+
+        // If we're still here, it may be a custom field
+        if ($column = CustomField::find($column)) {
+            return $column->name;
+        }
+
+        // Nothing!
+        return '';
+    }
+
+    /**
+     * Returns the content for the ticket listing field.
+     *
+     * @param string $column The column to get the content for.
+     * @param object $ticket The ticket data object.
+     *
+     * @return mixed
+     */
+    public static function dataFor($column, $ticket) {
+        switch ($column) {
+            // Ticket ID column
+            case 'ticket_id':
+                return $ticket->ticket_id;
+                break;
+
+            // Summary column
+            case 'summary':
+                return $ticket->summary;
+                break;
+
+            // Status column
+            case 'status':
+                return $ticket->status()->name;
+                break;
+
+            // Owner / author column
+            case 'owner':
+                return $ticket->user()->username;
+                break;
+
+            // Ticket type column
+            case 'type':
+                return $ticket->type()->name;
+                break;
+
+            // Component column
+            case 'component':
+                return $ticket->component() ? $ticket->component()->name : '';
+                break;
+
+            // Milestone column
+            case 'milestone':
+                return $ticket->milestone() ? $ticket->milestone()->name : '';
+                break;
+
+            // Updates column
+            case 'updates':
+                return $ticket->history()->execute()->rowCount();
+                break;
+
+            // Created at
+            case 'created_at':
+                return Language::translate('time.x_ago', [
+                    Time::agoInWords($ticket->created_at, false)
+                ]);
+                break;
+
+            // Updated at
+            case 'updated_at':
+                if ($ticket->updated_at) {
+                    return Language::translate('time.x_ago', [
+                        Time::agoInWords($ticket->updated_at, false)
+                    ]);
+                } else {
+                    Language::translate('never');
+                }
+                break;
+
+            // Votes
+            case 'votes':
+                return $ticket->votes;
+                break;
+
+            // Priority
+            case 'priority':
+                return $ticket->priority->name;
+                break;
+
+            // Severity
+            case 'severity':
+                return $ticket->severity->name;
+                break;
+        }
+
+        // If we're still here, it may be a custom field
+        if ($value = $ticket->customFieldValue($column)) {
+            return $value->value;
+        }
+
+        // Nothing!
+        return '';
     }
 }
