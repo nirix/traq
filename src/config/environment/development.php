1e8161deca4e3bd057c4857dbe06fe2683a02f87
Jack Polgar <jack@polgar.id.au>
2014-11-03 01:01:44 +1100

    Don't use fancy error reporting for overlays.

--- src/config/environment/development.php
+++ src/config/environment/development.php
@@ -1,4 +1,6 @@
 <?php
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
+if (!isset($_SERVER['HTTP_X_OVERLAY'])) {
+    $whoops = new \Whoops\Run;
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    $whoops->register();
+}
