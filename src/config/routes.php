9950593c7182845f4e607ea502bb5d8e0a3fd62f
Jack Polgar <jack@polgar.id.au>
2015-01-11 23:02:37 +1100

    Add route names and update token format.

--- src/config/routes.php
+++ src/config/routes.php
@@ -36,65 +36,110 @@ Router::map(function($r){
     $r->addToken('revision',     '(?P<revision>[\d]+)');
 
     $r->root("{$traq}\Projects::index");
-    $r->get('/admin')->to("{$traq}\Admin\Dashboard::index");
+    $r->get('/admin', 'admin')->to("{$traq}\Admin\Dashboard::index");
 
     $r->route('404')->to("{$traq}\Errors::notFound");
-    $r->get('/_js')->to("{$traq}\Misc::javascript");
+    $r->get('/_js', 'internal_js')->to("{$traq}\Misc::javascript");
 
     // --------------------------------------------------
     // Users
-    $r->get('/login')->to("{$traq}\Sessions::new");
+    $r->get('/login', 'login')->to("{$traq}\Sessions::new");
     $r->post('/login')->to("{$traq}\Sessions::create");
-    $r->get('/logout')->to("{$traq}\Sessions::destroy");
+    $r->get('/logout', 'logout')->to("{$traq}\Sessions::destroy");
 
-    $r->get('/register')->to("{$traq}\Users::new");
+    $r->get('/register', 'register')->to("{$traq}\Users::new");
     $r->post('/register')->to("{$traq}\Users::create");
 
-    $r->get('/usercp')->to("{$traq}\UserCP::index");
+    $r->get('/usercp', 'usercp')->to("{$traq}\UserCP::index");
     $r->post('/usercp')->to("{$traq}\UserCP::save");
 
     // --------------------------------------------------
     // Projects
-    $r->get("/projects")->to("{$traq}\Projects::index");
-    $r->get('/:project_slug')->to("{$traq}\Projects::show");
+    $r->get("/projects", 'projects')->to("{$traq}\Projects::index");
+    $r->get('/{project_slug}', 'show_project')->to("{$traq}\Projects::show");
 
+    // --------------------------------------------------
     // Timeline
-    $r->route('/:project_slug/timeline')->to("{$traq}\Timeline::index")
+    $r->route('/{project_slug}/timeline', 'timeline')->to("{$traq}\Timeline::index")
         ->method(['get','post']);
-    $r->get('/:project_slug/timeline/(?P<event_id>[\d]+)/delete')->to("{$traq}\Timeline::deleteEvent");
+    $r->get('/{project_slug}/timeline/{event_id}/delete', 'delete_timeline_event')
+        ->to("{$traq}\Timeline::deleteEvent");
 
+    // --------------------------------------------------
     // Roadmap
-    $r->get('/:project_slug/roadmap')->to("{$traq}\Roadmap::index");
-    $r->get('/:project_slug/roadmap/all')->to("{$traq}\Roadmap::index", ['filter' => 'all']);
-    $r->get('/:project_slug/roadmap/completed')->to("{$traq}\Roadmap::index", ['filter' => 'completed']);
-    $r->get('/:project_slug/roadmap/cancelled')->to("{$traq}\Roadmap::index", ['filter' => 'cancelled']);
-    $r->get('/:project_slug/milestone/:slug')->to("{$traq}\Roadmap::show");
+    $r->get('/{project_slug}/roadmap', 'roadmap')
+        ->to("{$traq}\Roadmap::index");
 
+    $r->get('/{project_slug}/roadmap/all', 'roadmap_all')
+        ->to("{$traq}\Roadmap::index", ['filter' => 'all']);
+
+    $r->get('/{project_slug}/roadmap/completed', 'roadmap_completed')
+        ->to("{$traq}\Roadmap::index", ['filter' => 'completed']);
+
+    $r->get('/{project_slug}/roadmap/cancelled', 'roadmap_cancelled')
+        ->to("{$traq}\Roadmap::index", ['filter' => 'cancelled']);
+
+    $r->get('/{project_slug}/milestone/{slug}', 'show_milestone')
+        ->to("{$traq}\Roadmap::show");
+
+    // --------------------------------------------------
     // Issues
-    $r->get('/:project_slug/tickets')->to("{$traq}\TicketListing::index");
-    $r->get('/:project_slug/tickets/(?P<ticket_id>[\d]+)')->to("{$traq}\Tickets::show");
+    $r->get('/{project_slug}/tickets')->to("{$traq}\TicketListing::index");
+    $r->get('/{project_slug}/tickets/{ticket_id}')->to("{$traq}\Tickets::show");
 
-    $r->get('/:project_slug/issues')->to("{$traq}\TicketListing::index");
-    $r->get('/:project_slug/issues(?P<ticket_id>[\d]+)')->to("{$traq}\Tickets::show");
-    $r->post('/:project_slug/issues/set-columns')->to("{$traq}\TicketListing::setColumns");
-    $r->post('/:project_slug/issues/update-filters')->to("{$traq}\TicketListing::updateFilters");
+    $r->get('/{project_slug}/issues', 'issues')
+        ->to("{$traq}\TicketListing::index");
 
-    $r->get('/:project_slug/issues/new')->to("{$traq}\Tickets::new");
-    $r->post('/:project_slug/issues/new')->to("{$traq}\Tickets::create");
+    $r->get('/{project_slug}/issues/{ticket_id}', 'show_issue')
+        ->to("{$traq}\Tickets::show");
 
+    $r->get('/{project_slug}/issues/new', 'new_issue')->to("{$traq}\Tickets::new");
+    $r->post('/{project_slug}/issues/new')->to("{$traq}\Tickets::create");
+
+    // --------------------------------------------------
+    // Issue filters
+    $r->post('/{project_slug}/issues/set-columns', 'set_issue_filter_columns')
+        ->to("{$traq}\TicketListing::setColumns");
+
+    $r->post('/{project_slug}/issues/update-filters', 'update_issue_filters')
+        ->to("{$traq}\TicketListing::updateFilters");
+
+    // --------------------------------------------------
     // Wiki
-    $r->get('/:project_slug/wiki')->to("{$traq}\Wiki::show", ['slug' => 'main']);
-    $r->get('/:project_slug/wiki/_new')->to("{$traq}\Wiki::new");
-    $r->post('/:project_slug/wiki/_new')->to("{$traq}\Wiki::create");
-    $r->get('/:project_slug/wiki/_pages')->to("{$traq}\Wiki::pages");
-    $r->get('/:project_slug/wiki/:wiki_slug')->to("{$traq}\Wiki::show");
-    $r->get('/:project_slug/wiki/:wiki_slug/_edit')->to("{$traq}\Wiki::edit");
-    $r->post('/:project_slug/wiki/:wiki_slug/_edit')->to("{$traq}\Wiki::save");
-    $r->get('/:project_slug/wiki/:wiki_slug/_revisions')->to("{$traq}\Wiki::revisions");
-    $r->get('/:project_slug/wiki/:wiki_slug/_revisions/(?<revision>[\d]+)')->to("{$traq}\Wiki::revision");
+    $r->get('/{project_slug}/wiki', 'wiki')
+        ->to("{$traq}\Wiki::show", ['slug' => 'main']);
 
+    // New page
+    $r->get('/{project_slug}/wiki/_new', 'new_wiki_page')
+        ->to("{$traq}\Wiki::new");
+
+    $r->post('/{project_slug}/wiki/_new')->to("{$traq}\Wiki::create");
+
+    // Pages listing
+    $r->get('/{project_slug}/wiki/_pages', 'wiki_pages')
+        ->to("{$traq}\Wiki::pages");
+
+    // Show page
+    $r->get('/{project_slug}/wiki/{slug}', 'show_wiki_page')
+        ->to("{$traq}\Wiki::show");
+
+    // Edit page
+    $r->get('/{project_slug}/wiki/{slug}/_edit', 'edit_wiki_page')
+        ->to("{$traq}\Wiki::edit");
+
+    $r->post('/{project_slug}/wiki/{slug}/_edit')->to("{$traq}\Wiki::save");
+
+    // List revisions
+    $r->get('/{project_slug}/wiki/{slug}/_revisions', 'wiki_page_revisions')
+        ->to("{$traq}\Wiki::revisions");
+
+    // Show revision
+    $r->get('/{project_slug}/wiki/{slug}/_revisions/{revision}', 'show_wiki_page_revision')
+        ->to("{$traq}\Wiki::revision");
+
+    // --------------------------------------------------
     // Changelog
-    $r->get('/:project_slug/changelog')->to("{$traq}\Projects::changelog");
+    $r->get('/{project_slug}/changelog', 'changelog')->to("{$traq}\Projects::changelog");
 
     // --------------------------------------------------
     // AdminCP
@@ -102,11 +147,11 @@ Router::map(function($r){
     // Projects
     $r->get('/admin/projects')->to("{$traq}\Admin\Projects::index");
     $r->get('/admin/projects/new')->to("{$traq}\Admin\Projects::new");
-    $r->get('/admin/projects/:project_id/edit')->to("{$traq}\Admin\Projects::edit");
-    $r->get('/admin/projects/:project_id/delete')->to("{$traq}\Admin\Projects::delete");
+    $r->get('/admin/projects/{id}/edit')->to("{$traq}\Admin\Projects::edit");
+    $r->get('/admin/projects/{id}/delete')->to("{$traq}\Admin\Projects::delete");
 
     $r->post('/admin/projects/new')->to("{$traq}\Admin\Projects::create");
-    $r->post('/admin/projects/:project_id/edit')->to("{$traq}\Admin\Projects::save");
+    $r->post('/admin/projects/{id}/edit')->to("{$traq}\Admin\Projects::save");
 
     // Plugins
     $r->get('/admin/plugins')->to("{$traq}\Admin\Plugins::index");
@@ -119,22 +164,22 @@ Router::map(function($r){
     $r->get('/admin/statuses')->to("{$traq}\Admin\Statuses::index");
     $r->get('/admin/statuses/new')->to("{$traq}\Admin\Statuses::new");
     $r->post('/admin/statuses/new')->to("{$traq}\Admin\Statuses::create");
-    $r->get('/admin/statuses/:id/edit')->to("{$traq}\Admin\Statuses::edit");
-    $r->post('/admin/statuses/:id/edit')->to("{$traq}\Admin\Statuses::save");
-    $r->get('/admin/statuses/:id/delete')->to("{$traq}\Admin\Statuses::destroy");
+    $r->get('/admin/statuses/{id}/edit')->to("{$traq}\Admin\Statuses::edit");
+    $r->post('/admin/statuses/{id}/edit')->to("{$traq}\Admin\Statuses::save");
+    $r->get('/admin/statuses/{id}/delete')->to("{$traq}\Admin\Statuses::destroy");
 
     // Priorities
     $r->get('/admin/priorities')->to("{$traq}\Admin\Priorities::index");
     $r->get('/admin/priorities/new')->to("{$traq}\Admin\Priorities::new");
     $r->post('/admin/priorities/new')->to("{$traq}\Admin\Priorities::create");
-    $r->get('/admin/priorities/:id/edit')->to("{$traq}\Admin\Priorities::edit");
-    $r->post('/admin/priorities/:id/edit')->to("{$traq}\Admin\Priorities::save");
-    $r->get('/admin/priorities/:id/delete')->to("{$traq}\Admin\Priorities::destroy");
+    $r->get('/admin/priorities/{id}/edit')->to("{$traq}\Admin\Priorities::edit");
+    $r->post('/admin/priorities/{id}/edit')->to("{$traq}\Admin\Priorities::save");
+    $r->get('/admin/priorities/{id}/delete')->to("{$traq}\Admin\Priorities::destroy");
 
     // Severities
     $r->get('/admin/severities')->to("{$traq}\Admin\Severities::index");
     $r->get('/admin/severities/new')->to("{$traq}\Admin\Severities::new");
     $r->post('/admin/severities/new')->to("{$traq}\Admin\Severities::create");
-    $r->get('/admin/severities/:id/edit')->to("{$traq}\Admin\Severities::edit");
-    $r->post('/admin/severities/:id/edit')->to("{$traq}\Admin\Severities::save");
+    $r->get('/admin/severities/{id}/edit')->to("{$traq}\Admin\Severities::edit");
+    $r->post('/admin/severities/{id}/edit')->to("{$traq}\Admin\Severities::save");
 });
