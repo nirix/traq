fa01b8d2b4276b316f6a837cfedb1595fc4a62f5
Jack Polgar <jack@polgar.id.au>
2015-01-21 14:58:54 +1100

    Merge config and database config into one file.

--- config/config.default.php
+++ config/config.default.php
@@ -1,4 +1,17 @@
 <?php
 return [
-    'environment' => "production"
+    // Environment
+    // Set to `development` for debugging.
+    'environment' => "production",
+
+    // Database config
+    'database' => [
+        // MySQL connection
+        'driver'   => 'pdo_mysql', // Database type.
+        'host'     => 'localhost', // Database server.
+        'user'     => 'root',      // Database username.
+        'password' => 'root',      // Database password.
+        'dbname'   => 'traq',      // Database name.
+        //'port'   => 3306         // Database server port
+    ]
 ];
