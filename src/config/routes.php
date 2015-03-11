f74f82c2ad2fce2809a83753a51999ce45dfd5d9
Jack Polgar <jack@polgar.id.au>
2015-03-11 22:01:51 +1100

    Update routes to use Avalon.

--- src/config/routes.php
+++ src/config/routes.php
@@ -1,8 +1,8 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2014 Jack Polgar
- * Copyright (C) 2012-2014 Traq.io
+ * Copyright (C) 2009-2015 Jack Polgar
+ * Copyright (C) 2012-2015 Traq.io
  * https://github.com/nirix
  * http://traq.io
  *
@@ -21,9 +21,9 @@
  * along with Traq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use Radium\Routing\Router;
+use Avalon\Routing\Router;
 
-Router::map(function($r){
+Router::map(function($r) {
     Router::$extensions[] = '.js';
     Router::$extensions[] = '.txt';
 
