38432432c23435859bef2654240ec271619240a3
Jack Polgar <jack@polgar.id.au>
2015-03-13 09:45:16 +1100

    Update wiki controller.

--- src/Controllers/Wiki.php
+++ src/Controllers/Wiki.php
@@ -1,10 +1,10 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2014 Jack Polgar
- * Copyright (C) 2012-2014 Traq.io
+ * Copyright (C) 2009-2015 Jack Polgar
+ * Copyright (C) 2012-2015 Traq.io
  * https://github.com/nirix
- * http://traq.io
+ * https://traq.io
  *
  * This file is part of Traq.
  *
@@ -23,8 +23,8 @@
 
 namespace Traq\Controllers;
 
-use Radium\Http\Request;
-use Radium\Http\Response;
+use Avalon\Http\Request;
+use Avalon\Http\Response;
 use Traq\Models\WikiPage;
 use Traq\Models\WikiRevision;
 use Traq\Models\Timeline;
