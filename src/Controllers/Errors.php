e44551081a75c9253b4e09dfc3255db297e61a0d
Jack Polgar <jack@polgar.id.au>
2015-03-13 09:37:28 +1100

    Fix PSR issues with Errors controller.

--- src/Controllers/Errors.php
+++ src/Controllers/Errors.php
@@ -1,10 +1,10 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2014 Jack Polgar
- * Copyright (C) 2012-2014 Traq.io
+ * Copyright (C) 2009-2015 Jack Polgar
+ * Copyright (C) 2012-2015 Traq.io
  * https://github.com/nirix
- * http://traq.io
+ * https://traq.io
  *
  * This file is part of Traq.
  *
@@ -34,7 +34,8 @@ class Errors extends AppController
     /**
      * 404 Error page.
      */
-    public function notFoundAction() {
+    public function notFoundAction()
+    {
         return $this->show404();
     }
 }
