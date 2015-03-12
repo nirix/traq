dc2869720ac2f82f516e6b0bd35d9d5556381e2e
Jack Polgar <jack@polgar.id.au>
2015-03-13 09:45:10 +1100

    Update errors helper.

--- src/Helpers/Errors.php
+++ src/Helpers/Errors.php
@@ -1,10 +1,10 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2014 Jack Polgar
- * Copyright (C) 2012-2014 Traq.io
+ * Copyright (C) 2009-2015 Jack Polgar
+ * Copyright (C) 2012-2015 Traq.io
  * https://github.com/nirix
- * http://traq.io
+ * https://traq.io
  *
  * This file is part of Traq.
  *
@@ -23,8 +23,8 @@
 
 namespace Traq\Helpers;
 
-use Radium\Templating\View;
-use Radium\Language;
+use Avalon\Templating\View;
+use Avalon\Language;
 
 /**
  * Error helpers.
