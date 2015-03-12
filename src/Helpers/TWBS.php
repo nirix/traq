626926d382906b8a1ceb6074225d2356784773e8
Jack Polgar <jack@polgar.id.au>
2015-03-13 09:35:32 +1100

    Update TWBS helper.

--- src/Helpers/TWBS.php
+++ src/Helpers/TWBS.php
@@ -1,10 +1,10 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2014 Jack Polgar
- * Copyright (C) 2012-2014 Traq.io
+ * Copyright (C) 2009-2015 Jack Polgar
+ * Copyright (C) 2012-2015 Traq.io
  * https://github.com/nirix
- * http://traq.io
+ * https://traq.io
  *
  * This file is part of Traq.
  *
@@ -23,9 +23,9 @@
 
 namespace Traq\Helpers;
 
-use Radium\Action\View;
-use Radium\Helpers\HTML;
-use Radium\Language;
+use Avalon\Action\View;
+use Avalon\Helpers\HTML;
+use Avalon\Language;
 
 /**
  * Twitter Bootstrap Helper.
