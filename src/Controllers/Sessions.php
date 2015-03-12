ea2ad68cfa5fa18c4b81bd3b8df4e9c837a25e0d
Jack Polgar <jack@polgar.id.au>
2015-03-13 09:38:10 +1100

    Update sessions controller to use Avalon.

--- src/Controllers/Sessions.php
+++ src/Controllers/Sessions.php
@@ -1,10 +1,10 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2014 Jack Polgar
- * Copyright (C) 2012-2014 Traq.io
+ * Copyright (C) 2009-2015 Jack Polgar
+ * Copyright (C) 2012-2015 Traq.io
  * https://github.com/nirix
- * http://traq.io
+ * https://traq.io
  *
  * This file is part of Traq.
  *
@@ -23,7 +23,7 @@
 
 namespace Traq\Controllers;
 
-use Radium\Http\Request;
+use Avalon\Http\Request;
 use Traq\Models\User;
 
 /**
@@ -39,7 +39,7 @@ class Sessions extends AppController
     {
         parent::__construct();
 
-        $this->before(['new', 'create'], function(){
+        $this->before(['new', 'create'], function() {
             if (LOGGEDIN) {
                 $this->redirectTo('/');
             }
@@ -49,8 +49,9 @@ class Sessions extends AppController
     /**
      * Login form
      */
-    public function newAction() {
-        return $this->respondTo(function($format){
+    public function newAction()
+    {
+        return $this->respondTo(function($format) {
             if ($format == 'html') {
                 if ($this->isOverlay) {
                     return $this->render('sessions/new.overlay.phtml');
