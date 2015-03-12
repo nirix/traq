ee2d127a9dbf927dbc889d5a0989ccdf1f4dcaf3
Jack Polgar <jack@polgar.id.au>
2015-03-13 09:39:03 +1100

    Update config example.

--- config/config.default.php
+++ config/config.default.php
@@ -6,12 +6,14 @@ return [
 
     // Database config
     'database' => [
-        // MySQL connection
-        'driver'   => 'pdo_mysql', // Database type.
-        'host'     => 'localhost', // Database server.
-        'user'     => 'root',      // Database username.
-        'password' => 'root',      // Database password.
-        'dbname'   => 'traq',      // Database name.
-        //'port'   => 3306         // Database server port
+        'production' => [
+            // MySQL connection
+            'driver'   => 'pdo_mysql', // Database type.
+            'host'     => 'localhost', // Database server.
+            'user'     => 'root',      // Database username.
+            'password' => 'root',      // Database password.
+            'dbname'   => 'traq',      // Database name.
+            //'port'   => 3306         // Database server port
+        ]
     ]
 ];
