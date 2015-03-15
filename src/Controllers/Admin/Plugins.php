58b066dd0e2e493ab817b33916f63b6bae0262df
Jack Polgar <jack@polgar.id.au>
2015-03-16 09:45:59 +1100

    Update plugin management controller.

--- src/Controllers/Admin/Plugins.php
+++ src/Controllers/Admin/Plugins.php
@@ -1,8 +1,8 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2014 Jack Polgar
- * Copyright (C) 2012-2014 Traq.io
+ * Copyright (C) 2009-2015 Jack Polgar
+ * Copyright (C) 2012-2015 Traq.io
  * https://github.com/nirix
  * http://traq.io
  *
@@ -23,8 +23,7 @@
 
 namespace Traq\Controllers\Admin;
 
-use Radium\Http\Request;
-
+use Avalon\Http\Request;
 use Traq\Traq;
 use Traq\Models\Plugin;
 use Traq\Plugin\Registry;
@@ -99,7 +98,7 @@ class Plugins extends AppController
             (new Plugin($info))->save();
         }
 
-        $this->redirectTo('/admin/plugins');
+        return $this->redirectTo('/admin/plugins');
     }
 
     /**
@@ -119,7 +118,7 @@ class Plugins extends AppController
         }
 
         $plugin->delete();
-        $this->redirectTo('/admin/plugins');
+        return $this->redirectTo('/admin/plugins');
     }
 
     /**
@@ -138,7 +137,7 @@ class Plugins extends AppController
             $plugin->save();
         }
 
-        $this->redirectTo('/admin/plugins');
+        return $this->redirectTo('/admin/plugins');
     }
 
     /**
@@ -155,6 +154,6 @@ class Plugins extends AppController
             $plugin->save();
         }
 
-        $this->redirectTo('/admin/plugins');
+        return $this->redirectTo('/admin/plugins');
     }
 }
