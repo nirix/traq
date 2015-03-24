6402086bb0b3c37845b275d120671bd5f92f4d7b
Jack Polgar <jack@polgar.id.au>
2015-03-24 17:08:48 +1100

    Move installation error alert to partial view.

--- src/Installer/Kernel.php
+++ src/Installer/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends AppKernel
     {
         parent::__construct();
 
+        class_alias("Avalon\\Templating\\View", "View");
         class_alias("Avalon\\Http\\Request", "Request");
         class_alias("Avalon\\Helpers\\HTML", "HTML");
     }
