e48ae38d8429eaa40561d5b888821d7f830cbfbe
Jack Polgar <jack@polgar.id.au>
2015-05-27 19:25:39 +1000

    Add UserCP specific layout.

--- src/Controllers/UserCP.php
+++ src/Controllers/UserCP.php
@@ -1,10 +1,10 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2014 Jack Polgar
- * Copyright (C) 2012-2014 Traq.io
+ * Copyright (C) 2009-2015 Jack Polgar
+ * Copyright (C) 2012-2015 Traq.io
  * https://github.com/nirix
- * http://traq.io
+ * https://traq.io
  *
  * This file is part of Traq.
  *
@@ -23,20 +23,19 @@
 
 namespace Traq\Controllers;
 
-use Radium\Http\Request;
-use Radium\Action\View;
-
 use Traq\Models\Subscription;
 
 /**
  * UserCP controller
  *
  * @author Jack P.
- * @since 3.0
+ * @since 3.0.0
  * @package Traq\Controllers
  */
 class UserCP extends AppController
 {
+    protected $layout = "usercp.phtml";
+
     public function __construct()
     {
         parent::__construct();
