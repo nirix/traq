42105bc0390be6ceb874d005620951b30512caf5
Jack Polgar <jack@polgar.id.au>
2015-12-22 00:40:13 +1100

    Remove old JavaScript view.

--- src/views/javascript.php
+++
@@ -1,9 +0,0 @@
-window.traq.uri = '<?=Request::basePath()?>';
-
-// Yes and No translations
-window.traq.locale = {
-    confirm: {
-        'yes': '<?=$t('confirm.yes')?>',
-        'no': '<?=$t('confirm.no')?>'
-    }
-}
