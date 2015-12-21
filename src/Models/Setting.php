758cbaf79b97c2b8188a3204cd3715fce1a6930f
Jack Polgar <jack@polgar.id.au>
2015-12-22 00:36:19 +1100

    Update setting model.

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -1,8 +1,8 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2014 Jack Polgar
- * Copyright (C) 2012-2014 Traq.io
+ * Copyright (C) 2009-2015 Jack P.
+ * Copyright (C) 2012-2015 Traq.io
  * https://github.com/nirix
  * http://traq.io
  *
@@ -32,12 +32,14 @@ use Avalon\Database\Model;
  */
 class Setting extends Model
 {
+    protected static $_tableAlias = 'st';
+
     protected static $_validates = array(
         'setting' => array('required')
     );
 
     public static function get($setting)
     {
-        return static::where('setting = ?', $setting)->fetch();
+        return static::where('st.setting = ?')->setParameter(0, $setting)->fetch();
     }
 }
