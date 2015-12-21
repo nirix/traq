b42a3470fc9a663ab19cd97d1c0aca666732d130
Jack Polgar <jack@polgar.id.au>
2015-12-21 23:54:30 +1100

    Bare minimum version file.

--- src/version.php
+++ src/version.php
@@ -1,40 +1,4 @@
 <?php
-/*!
- * Traq
- * Copyright (C) 2009-2014 Jack Polgar
- * Copyright (C) 2012-2014 Traq.io
- * https://github.com/nirix
- * http://traq.io
- *
- * This file is part of Traq.
- *
- * Traq is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 3 only.
- *
- * Traq is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Traq. If not, see <http://www.gnu.org/licenses/>.
- */
-
-if (defined("TRAQ_VERSION")) {
-    return;
-}
-
-// Version
-const TRAQ_VERSION = "4.0.0-dev";
-
-// Version code
-// 1.2.3  -> 10203
-// 1.3.0  -> 10300
-// 1.12.1 -> 11201
-// 1.2.11 -> 10211
-const TRAQ_VERSION_ID    = 40000; // Used to check for new versions
-const TRAQ_DB_VERSION_ID = 40000; // Database version
-
-// API version
-const TRAQ_API_VERSION = 2.0;
+const TRAQ_VERSION     = '4.0.0-dev-2';
+const TRAQ_VERSION_ID  = 40000;
+const TRAQ_API_VERSION = '2.0.0';
