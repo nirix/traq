b2fab9fe71007e89aaec7641c39d87cce40ea15c
Jack Polgar <jack@polgar.id.au>
2015-12-22 19:53:12 +1100

    Add `UserActivationCode` model.

---
+++ src/Models/UserActivationCode.php
@@ -0,0 +1,44 @@
+<?php
+/*!
+ * Traq
+ * Copyright (C) 2009-2015 Jack P.
+ * Copyright (C) 2012-2015 Traq.io
+ * https://github.com/nirix
+ * https://traq.io
+ *
+ * This file is part of Traq.
+ *
+ * Traq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ *
+ * Traq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Traq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Traq\Models;
+
+use Avalon\Database\Model;
+
+/**
+ * User activation code model.
+ *
+ * @package Traq\Models
+ * @author Jack P.
+ * @since 4.0.0
+ */
+class UserActivationCode extends Model
+{
+    public static function get($type, $code)
+    {
+        return static::select('*')->where('type = ?')->andWhere('activation_code = ?')
+            ->setParameter(0, $type)
+            ->setParameter(1, $code);
+            ->fetch();
+    }
+}
