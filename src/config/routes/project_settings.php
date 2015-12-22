4f1eb547178436a711704678aa0c5e1fe56df1b1
Jack Polgar <jack@polgar.id.au>
2015-12-23 00:10:03 +1100

    Add milestone management routes.

--- src/config/routes/project_settings.php
+++ src/config/routes/project_settings.php
@@ -7,6 +7,11 @@ Router::post('project_settings_save', '/{pslug}/settings', "{$ns}\\ProjectSettin
 
 // Milestones
 Router::get('project_settings_milestones', '/{pslug}/settings/milestones', "{$ns}\\ProjectSettings\\Milestones::index");
+Router::get('project_settings_new_milestone', '/{pslug}/settings/milestones/new', "{$ns}\\ProjectSettings\\Milestones::new");
+Router::post('project_settings_create_milestone', '/{pslug}/settings/milestones/new', "{$ns}\\ProjectSettings\\Milestones::create");
+Router::get('project_settings_edit_milestone', '/{pslug}/settings/milestones/{id}/edit', "{$ns}\\ProjectSettings\\Milestones::edit");
+Router::post('project_settings_save_milestone', '/{pslug}/settings/milestones/{id}/edit', "{$ns}\\ProjectSettings\\Milestones::save");
+Router::get('project_settings_delete_milestone', '/{pslug}/settings/milestones/{id}/delete', "{$ns}\\ProjectSettings\\Milestones::destroy");
 
 // Components
 Router::get('project_settings_components', '/{pslug}/settings/components', "{$ns}\\ProjectSettings\\Components::index");
