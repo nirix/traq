8eee8c75d53fb7e36ed854498dbe3ba777fb9b7b
Jack Polgar <nrx@nirix.net>
2016-01-01 23:16:33 +1100

    Redirect to installer if config file doesn't exist.

--- index.php
+++ index.php
@@ -4,4 +4,9 @@ $autoloader = require __DIR__ . '/vendor/autoload.php';
 define('START_TIME', microtime(true));
 define('START_MEM', memory_get_usage());
 
+if (!file_exists(__DIR__ . '/config/config.php')) {
+    header("Location: ./install");
+    exit;
+}
+
 (new Traq\Kernel)->run();
