57d903047b3f033624b134dde219b909aed8c6bc
Jack Polgar <nrx@nirix.net>
2016-01-02 23:15:09 +1100

    Update ticket model data types.

--- src/Models/Ticket.php
+++ src/Models/Ticket.php
@@ -1,8 +1,8 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2015 Jack P.
- * Copyright (C) 2012-2015 Traq.io
+ * Copyright (C) 2009-2016 Jack P.
+ * Copyright (C) 2012-2016 Traq.io
  * https://github.com/nirix
  * https://traq.io
  *
@@ -44,6 +44,7 @@ class Ticket extends Model
     ];
 
     protected static $_dataTypes = [
-        'tasks' => 'json_array'
+        'tasks' => 'json_array',
+        'extra' => 'json_array'
     ];
 }
