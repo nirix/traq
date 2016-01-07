634ad01ad8d8a4aee8b835bd038c042515865c80
Jack Polgar <nrx@nirix.net>
2016-01-08 10:03:22 +1100

    We don't need to select the ticket body.

--- src/Controllers/TicketListing.php
+++ src/Controllers/TicketListing.php
@@ -56,7 +56,6 @@ class TicketListing extends AppController
         // Only get the current projects tickets
         $tickets = ticketQuery()
             ->where('t.project_id = ?')
-            ->addSelect('t.body')
             ->setParameter(0, $this->currentProject['id']);
 
         // Sort tickets by the projects sorting setting or by the users selection
