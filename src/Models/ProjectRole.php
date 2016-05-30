575f16020028ed4e14710711d8effab4a393abff
Jack Polgar <jack@polgar.id.au>
2016-05-31 01:50:55 +1000

    Update `Priority`, `ProjectRole` and `Severity` models.

--- src/Models/ProjectRole.php
+++ src/Models/ProjectRole.php
@@ -1,10 +1,10 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2014 Jack P.
- * Copyright (C) 2012-2014 Traq.io
+ * Copyright (C) 2009-2016 Jack P.
+ * Copyright (C) 2012-2016 Traq.io
  * https://github.com/nirix
- * http://traq.io
+ * https://traq.io
  *
  * This file is part of Traq.
  *
@@ -23,10 +23,8 @@
 
 namespace Traq\Models;
 
-use Avalon\Database\Model;
-
 /**
- * Project roles model.
+ * Project role model.
  *
  * @package Traq\Models
  * @author Jack P.
