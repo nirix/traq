5bb8f202bf35d3f38297713814aac86a083e3ca4
Jack Polgar <jack@polgar.id.au>
2016-06-21 03:51:24 +1000

    Add routes used in AdminCP navbar.

--- src/config/routes/admin.php
+++ src/config/routes/admin.php
@@ -18,3 +18,30 @@ Router::post('admin_create_project', '/admin/projects', "{$ans}Projects::create"
 Router::get('admin_edit_project', '/admin/projects/{id}/edit', "{$ans}Projects::edit");
 Router::put('admin_save_project', '/admin/projects/{id}', "{$ans}Projects::save")->method(['PUT', 'PATCH']);
 Router::delete('admin_delete_project', '/admin/projects/{id}/delete', "{$ans}Projects::destroy");
+
+// Project Roles
+Router::get('admin_project_roles', '/admin/project-roles', "{$ans}ProjectRoles::index");
+
+// Users
+Router::get('admin_users', '/admin/users', "{$ans}Users::index");
+
+// Groups
+Router::get('admin_groups', '/admin/usergroups', "{$ans}Groups::index");
+
+// Plugins
+Router::get('admin_plugins', '/admin/plugins', "{$ans}Plugins::index");
+
+// Types
+Router::get('admin_types', '/admin/types', "{$ans}Types::index");
+
+// Statuses
+Router::get('admin_statuses', '/admin/statuses', "{$ans}Statuses::index");
+
+// Priorities
+Router::get('admin_priorities', '/admin/priorities', "{$ans}Priorities::index");
+
+// Severities
+Router::get('admin_severities', '/admin/severities', "{$ans}Severities::index");
+
+// Permissions
+Router::get('admin_permissions', '/admin/permissions/usergroups', "{$ans}Permissions::usergroups");
