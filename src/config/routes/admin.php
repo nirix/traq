75f4897976358c2ec0a1e7c6fb2fee724e14c51e
Jack Polgar <jack@polgar.id.au>
2016-06-21 18:29:47 +1000

    Add dividers between admin routes.

--- src/config/routes/admin.php
+++ src/config/routes/admin.php
@@ -7,10 +7,12 @@ $ans = "{$ns}Admin\\";
 // Admin routes
 Router::get('admincp', '/admin', "{$ans}Dashboard::index");
 
+// -----------------------------------------------------------------------------
 // Settings
 Router::get('admin_settings', '/admin/settings', "{$ans}Settings::index");
 Router::post('admin_settings_save', '/admin/settings', "{$ans}Settings::save");
 
+// -----------------------------------------------------------------------------
 // Projects
 Router::get('admin_projects', '/admin/projects', "{$ans}Projects::index");
 Router::get('admin_new_project', '/admin/projects/new', "{$ans}Projects::new");
@@ -19,6 +21,7 @@ Router::get('admin_edit_project', '/admin/projects/{id}/edit', "{$ans}Projects::
 Router::put('admin_save_project', '/admin/projects/{id}', "{$ans}Projects::save")->method(['PUT', 'PATCH']);
 Router::delete('admin_delete_project', '/admin/projects/{id}/delete', "{$ans}Projects::destroy");
 
+// -----------------------------------------------------------------------------
 // Project Roles
 Router::get('admin_project_roles', '/admin/project-roles', "{$ans}ProjectRoles::index");
 Router::get('admin_new_project_role', '/admin/projects-roles/new', "{$ans}ProjectRoles::new");
@@ -27,9 +30,11 @@ Router::get('admin_edit_project_role', '/admin/projects-roles/{id}/edit', "{$ans
 Router::put('admin_save_project_role', '/admin/projects-roles/{id}', "{$ans}ProjectRoles::save")->method(['PUT', 'PATCH']);
 Router::delete('admin_delete_project_role', '/admin/projects-roles/{id}/delete', "{$ans}ProjectRoles::destroy");
 
+// -----------------------------------------------------------------------------
 // Users
 Router::get('admin_users', '/admin/users', "{$ans}Users::index");
 
+// -----------------------------------------------------------------------------
 // Groups
 Router::get('admin_groups', '/admin/usergroups', "{$ans}Groups::index");
 Router::get('admin_new_group', '/admin/usergroups/new', "{$ans}Groups::new");
@@ -38,6 +43,7 @@ Router::get('admin_edit_group', '/admin/usergroups/{id}/edit', "{$ans}Groups::ed
 Router::put('admin_save_group', '/admin/usergroups/{id}', "{$ans}Groups::save")->method(['PUT', 'PATCH']);
 Router::delete('admin_delete_group', '/admin/usergroups/{id}/delete', "{$ans}Groups::destroy");
 
+// -----------------------------------------------------------------------------
 // Plugins
 Router::get('admin_plugins', '/admin/plugins', "{$ans}Plugins::index");
 Router::get('admin_plugins_install', '/admin/plugins/install', "{$ans}Plugins::install");
@@ -45,6 +51,7 @@ Router::get('admin_plugins_uninstall', '/admin/plugins/uninstall', "{$ans}Plugin
 Router::get('admin_plugins_enable', '/admin/plugins/enable', "{$ans}Plugins::enable");
 Router::get('admin_plugins_disable', '/admin/plugins/disable', "{$ans}Plugins::disable");
 
+// -----------------------------------------------------------------------------
 // Types
 Router::get('admin_types', '/admin/types', "{$ans}Types::index");
 Router::get('admin_new_type', '/admin/types/new', "{$ans}Types::new");
@@ -53,14 +60,18 @@ Router::get('admin_edit_type', '/admin/types/{id}/edit', "{$ans}Types::edit");
 Router::put('admin_save_type', '/admin/types/{id}', "{$ans}Types::save")->method(['PUT', 'PATCH']);
 Router::delete('admin_delete_type', '/admin/types/{id}/delete', "{$ans}Types::destroy");
 
+// -----------------------------------------------------------------------------
 // Statuses
 Router::get('admin_statuses', '/admin/statuses', "{$ans}Statuses::index");
 
+// -----------------------------------------------------------------------------
 // Priorities
 Router::get('admin_priorities', '/admin/priorities', "{$ans}Priorities::index");
 
+// -----------------------------------------------------------------------------
 // Severities
 Router::get('admin_severities', '/admin/severities', "{$ans}Severities::index");
 
+// -----------------------------------------------------------------------------
 // Permissions
 Router::get('admin_permissions', '/admin/permissions/usergroups', "{$ans}Permissions::usergroups");
