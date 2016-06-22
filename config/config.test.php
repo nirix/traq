20094437bf8a63c7e54bb8a88cbafb2a096430fc
Jack Polgar <jack@polgar.id.au>
2016-06-23 01:06:03 +1000

    Lets try testing with travis.

---
+++ config/config.test.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'environment' => "test",
+    'database' => [
+        'test' => [
+            'driver'   => 'pdo_mysql',
+            'host'     => '127.0.0.1',
+            'user'     => 'travis',
+            'password' => '',
+            'dbname'   => 'traq_test',
+            'prefix'   => 'traq_',
+            'charset'  => 'utf8'
+        ]
+    ],
+
+    'email' => [
+        'type' => "sendmail",
+        'path' => "/usr/bin/sendmail -bs"
+    ]
+];
