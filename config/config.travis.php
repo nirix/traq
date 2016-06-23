343ddda29bb9baaed7d138ffa9aa18a10bc31f77
Jack Polgar <jack@polgar.id.au>
2016-06-23 14:49:16 +1000

    Remove testing on PHP 5.4 and add PostgreSQL and SQLite.

---
+++ config/config.travis.php
@@ -0,0 +1,42 @@
+<?php
+$config = [
+    'environment' => 'test',
+    'db' => [
+        'test' => []
+    ],
+    'email' => [
+        'type' => "sendmail",
+        'path' => "/usr/bin/sendmail -bs"
+    ]
+];
+
+$db = [
+    'mysql' => [
+        'driver'   => 'pdo_mysql',
+        'host'     => '127.0.0.1',
+        'user'     => 'travis',
+        'password' => '',
+        'dbname'   => 'traq_test',
+        'prefix'   => 'test_',
+        'charset'  => 'utf8'
+    ],
+
+    'postgresql' => [
+        'driver'   => 'pdo_pgsql',
+        'host'     => '127.0.0.1',
+        'user'     => 'postgres',
+        'password' => '',
+        'dbname'   => 'traq_test',
+        'prefix'   => 'test_',
+        'charset'  => 'utf8'
+    ],
+
+    'sqlite' => [
+        'driver' => 'pdo_sqlite',
+        'memory' => true
+    ]
+];
+
+$config['db']['test'] = $db[getenv('DB')];
+
+return $config;
