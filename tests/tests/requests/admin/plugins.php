0b5418e1dc2374c24ec875128fc210299101755d
Jack Polgar <jack@polgar.id.au>
2016-06-26 13:34:14 +1000

    Add more plugin management tests.

--- tests/tests/requests/admin/plugins.php
+++ tests/tests/requests/admin/plugins.php
@@ -13,4 +13,56 @@ $testSuite->createGroup('Requests / Admin / Plugins', function ($g) {
         $t->assertEquals(200, $resp->status);
         $t->assertContains('<h1 class="page-header">Plugins</h1>', $resp->body);
     });
+
+    $g->test('Install plugin', function ($t) use ($admin) {
+        $resp = $t->visit('admin_plugins_install', [
+            'get' => [
+                'plugin' => 'traq/markdown-plugin'
+            ],
+            'cookie' => [
+                'traq' => $admin['session_hash']
+            ]
+        ]);
+
+        $t->assertRedirectTo($t->generateUrl('admin_plugins'), $resp);
+    });
+
+    $g->test('Disable plugin', function ($t) use ($admin) {
+        $resp = $t->visit('admin_plugins_disable', [
+            'get' => [
+                'plugin' => 'traq/markdown-plugin'
+            ],
+            'cookie' => [
+                'traq' => $admin['session_hash']
+            ]
+        ]);
+
+        $t->assertRedirectTo($t->generateUrl('admin_plugins'), $resp);
+    });
+
+    $g->test('Enable plugin', function ($t) use ($admin) {
+        $resp = $t->visit('admin_plugins_enable', [
+            'get' => [
+                'plugin' => 'traq/markdown-plugin'
+            ],
+            'cookie' => [
+                'traq' => $admin['session_hash']
+            ]
+        ]);
+
+        $t->assertRedirectTo($t->generateUrl('admin_plugins'), $resp);
+    });
+
+    $g->test('Uninstall plugin', function ($t) use ($admin) {
+        $resp = $t->visit('admin_plugins_uninstall', [
+            'get' => [
+                'plugin' => 'traq/markdown-plugin'
+            ],
+            'cookie' => [
+                'traq' => $admin['session_hash']
+            ]
+        ]);
+
+        $t->assertRedirectTo($t->generateUrl('admin_plugins'), $resp);
+    });
 });
