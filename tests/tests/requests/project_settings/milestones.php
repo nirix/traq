b8831597b581da4a318a6d4e06aab8ef0489cda6
Jack Polgar <jack@polgar.id.au>
2016-06-29 01:40:16 +1000

    Add tests for creating milestones.

--- tests/tests/requests/project_settings/milestones.php
+++ tests/tests/requests/project_settings/milestones.php
@@ -4,6 +4,7 @@ $testSuite->createGroup('Requests / Project Settings / Milestones', function ($g
     $manager = createProjectManager();
     $project = $manager->project();
     $user = $manager->user();
+    $milestone = createMilestone($project);
 
     $g->test('List milestones', function ($t) use ($project, $user) {
         $resp = $t->visit('project_settings_milestones', [
@@ -31,5 +32,39 @@ $testSuite->createGroup('Requests / Project Settings / Milestones', function ($g
 
         $t->assertEquals(200, $resp->status);
         $t->assertContains('<h1 class="page-header">New Milestone</h1>', $resp->body);
+    });
+
+    $g->test('Create milestone', function ($t) use ($project, $user) {
+        $resp = $t->visit('project_settings_create_milestone', [
+            'method' => 'POST',
+            'routeTokens' => [
+                'pslug' => $project['slug']
+            ],
+            'post' => [
+                'name' => '1.0-test-name',
+                'slug' => '1.0-test-slug'
+            ],
+            'cookie' => [
+                'traq' => $user['session_hash']
+            ]
+        ]);
+
+        $t->assertRedirectTo($t->generateUrl('project_settings_milestones'), $resp);
+    });
+
+    $g->test('Disallow editing another projects milestone', function ($t) use ($project, $user) {
+        $milestone = createMilestone();
+
+        $resp = $t->visit('project_settings_edit_milestone', [
+            'routeTokens' => [
+                'pslug' => $project['slug'],
+                'id' => $milestone['id']
+            ],
+            'cookie' => [
+                'traq' => $user['session_hash']
+            ]
+        ]);
+
+        $t->assertEquals(404, $resp->status);
     });
 });
