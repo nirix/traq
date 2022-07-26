fca39cdfa5388266c35c9cd8c9999571d6159b98
Jack Polgar <hello@jp.id.au>
2022-07-27 00:17:55 +1000

    Move FontAwesome js to separate file

--- traq-ui/main.ts
+++ traq-ui/main.ts
@@ -1,43 +1,7 @@
 import "./css/main.css"
+import "./main/FontAwesome"
 
 // Alpinejs for simpler UI tasks
 import Alpine from "alpinejs"
 window.Alpine = Alpine
 Alpine.start()
-
-// FontAwesome
-import { library, dom } from "@fortawesome/fontawesome-svg-core"
-import {
-  faPencil,
-  faTrash,
-  faEye,
-  faEyeSlash,
-  faBold,
-  faItalic,
-  faHeading,
-  faQuoteLeft,
-  faListUl,
-  faListOl,
-  faLink,
-  faImage,
-  faColumns,
-  faArrowsAlt,
-  faQuestionCircle,
-} from "@fortawesome/free-solid-svg-icons"
-
-library.add(faPencil)
-library.add(faTrash)
-library.add(faEye)
-library.add(faEyeSlash)
-library.add(faBold)
-library.add(faItalic)
-library.add(faHeading)
-library.add(faQuoteLeft)
-library.add(faListUl)
-library.add(faListOl)
-library.add(faLink)
-library.add(faImage)
-library.add(faColumns)
-library.add(faArrowsAlt)
-library.add(faQuestionCircle)
-dom.watch()
---
+++ traq-ui/main/FontAwesome.ts
@@ -0,0 +1,36 @@
+// FontAwesome
+import { library, dom } from "@fortawesome/fontawesome-svg-core"
+import {
+  faPencil,
+  faTrash,
+  faEye,
+  faEyeSlash,
+  faBold,
+  faItalic,
+  faHeading,
+  faQuoteLeft,
+  faListUl,
+  faListOl,
+  faLink,
+  faImage,
+  faColumns,
+  faArrowsAlt,
+  faQuestionCircle,
+} from "@fortawesome/free-solid-svg-icons"
+
+library.add(faPencil)
+library.add(faTrash)
+library.add(faEye)
+library.add(faEyeSlash)
+library.add(faBold)
+library.add(faItalic)
+library.add(faHeading)
+library.add(faQuoteLeft)
+library.add(faListUl)
+library.add(faListOl)
+library.add(faLink)
+library.add(faImage)
+library.add(faColumns)
+library.add(faArrowsAlt)
+library.add(faQuestionCircle)
+dom.watch()
