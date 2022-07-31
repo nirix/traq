89bc742a97e4b336864f300df90cea10280b167d
Jack Polgar <hello@jp.id.au>
2022-07-31 19:58:33 +1000

    Update form label width

--- src/Traq/Controllers/ProjectSettingsController.php
+++ src/Traq/Controllers/ProjectSettingsController.php
@@ -39,46 +39,51 @@ class ProjectSettingsController extends AppController
     public function index()
     {
         if (Request::method() == 'post') {
-            // Clone the project model so nothing
-            // funky happens when there are errors
-            // with the new information.
-            $project = clone $this->project;
-            View::set('proj', $project);
-
-            // Update the information
-            $project->set(array(
-                'name'         => Request::post('name', $project->name),
-                'slug'         => Request::post('slug', $project->slug),
-                'codename'     => Request::post('codename', $project->codename),
-                'info'         => Request::post('info', $project->info),
-                'displayorder' => Request::post('displayorder', $project->displayorder),
-                'default_ticket_type_id' => Request::post('default_ticket_type_id', $project->default_ticket_type_id),
-                'default_ticket_sorting' => Request::post('default_ticket_sorting', $project->default_ticket_sorting)
-            ));
-
-            // Set enable_wiki
-            if ($this->is_api) {
-                $project->enable_wiki = Request::post('enable_wiki', $project->enable_wiki);
-            } else {
-                $project->enable_wiki = Request::post('enable_wiki', 0);
-            }
-
-            // Check if the data is valid
-            if ($project->is_valid()) {
-                // Save and redirect
-                $project->save();
-
-                if ($this->is_api) {
-                    return \API::response(1, array('project' => $project));
-                } else {
-                    return Request::redirectTo($project->href('settings'));
-                }
-            } else {
-                return $this->renderView('project_settings/index.phtml');
-            }
+            return $this->save();
         }
 
         View::set('proj', $this->project);
+        return $this->renderView('project_settings/index.phtml');
+    }
+
+    public function save()
+    {
+        // Clone the project model so nothing
+        // funky happens when there are errors
+        // with the new information.
+        $project = clone $this->project;
+        View::set('proj', $project);
+
+        // Update the information
+        $project->set(array(
+            'name'         => Request::post('name', $project->name),
+            'slug'         => Request::post('slug', $project->slug),
+            'codename'     => Request::post('codename', $project->codename),
+            'info'         => Request::post('info', $project->info),
+            'displayorder' => Request::post('displayorder', $project->displayorder),
+            'default_ticket_type_id' => Request::post('default_ticket_type_id', $project->default_ticket_type_id),
+            'default_ticket_sorting' => Request::post('default_ticket_sorting', $project->default_ticket_sorting)
+        ));
+
+        // Set enable_wiki
+        if ($this->is_api) {
+            $project->enable_wiki = Request::post('enable_wiki', $project->enable_wiki);
+        } else {
+            $project->enable_wiki = Request::post('enable_wiki', 0);
+        }
+
+        // Check if the data is valid
+        if ($project->is_valid()) {
+            // Save and redirect
+            $project->save();
+
+            if ($this->is_api) {
+                return \API::response(1, array('project' => $project));
+            } else {
+                return Request::redirectTo($project->href('settings'));
+            }
+        }
+
         return $this->renderView('project_settings/index.phtml');
     }
 }
