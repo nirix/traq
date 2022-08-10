eb29ce2e07bd22532a27231e9655cd6dbaca6428
Jack Polgar <hello@jp.id.au>
2022-08-11 01:23:52 +1000

    Bump DB revision

--- vendor/traq/version.php
+++ vendor/traq/version.php
@@ -30,7 +30,7 @@ define("TRAQ_VER", "3.8.0"); // Pretty obvious...
 // 1.12.1  -> 11201
 // 1.2.11  -> 10211
 define("TRAQ_VER_CODE", 30800); // Used to check for new versions
-define("TRAQ_DB_VER", 30701);   // Database version
+define("TRAQ_DB_VER", 30800);   // Database version
 
 // API version
 define("TRAQ_API_VER", "1.1");
