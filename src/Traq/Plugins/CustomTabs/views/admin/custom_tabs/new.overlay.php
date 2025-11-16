f8d8e52ee4f45e0976bd4a5ca25b8a267d221541
Jack Polgar <hello@jp.id.au>
2025-11-17 00:10:10 +1100

    Update Custom Tab plugin views

--- src/Traq/Plugins/CustomTabs/views/admin/custom_tabs/new.overlay.php
+++ src/Traq/Plugins/CustomTabs/views/admin/custom_tabs/new.overlay.php
@@ -1,11 +1,13 @@
-<h3><?php echo l('new_tab'); ?></h3>
-<form action="<?php echo Request::requestUri(); ?>" method="post" class="overlay_thin">
+<h3><?= l('new_tab'); ?></h3>
+<form action="<?= Request::uri(); ?>" method="post" class="overlay_thin">
 	<?php echo show_errors($tab->errors); ?>
 	<div class="tabular box">
-		<?php echo View::render('custom_tabs/_form'); ?>
+		<?= view('custom_tabs/_form', [
+			'tab' => $tab,
+		]); ?>
 	</div>
 	<div class="actions">
-		<input type="submit" value="<?php echo l('create'); ?>" />
-		<input type="button" value="<?php echo l('cancel'); ?>" onclick="close_overlay();" />
+		<input type="submit" value="<?= l('create'); ?>" />
+		<input type="button" value="<?= l('cancel'); ?>" onclick="close_overlay();" />
 	</div>
 </form>
