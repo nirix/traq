6d53f6fbffe4889e093df18807cff8a69861fd1c
Jack Polgar <hello@jp.id.au>
2025-11-20 03:11:05 +1100

    Add sorting and filtering to user profile tickets

--- src/assets/main/ProfileTickets.ts
+++ src/assets/main/ProfileTickets.ts
@@ -22,7 +22,7 @@
 
 import axios from 'axios'
 import Alpine from 'alpinejs'
-import type { TicketInterface } from '../interfaces'
+import type { StatusInterface, TicketInterface, TypeInterface } from '../interfaces'
 
 Alpine.data('profileTickets', ({ userName, projectSlug }) => ({
     userName: userName,
@@ -32,22 +32,65 @@ Alpine.data('profileTickets', ({ userName, projectSlug }) => ({
     page: 1,
     totalPages: 1,
     open: true,
+    sortColumn: 'status',
+    sortOrder: 'asc',
+    type: [] as string[],
+    status: [] as string[],
+    statuses: [] as StatusInterface[],
+    types: [] as TypeInterface[],
 
     init() {
-        this.getTickets(this.page)
+        this.getTickets()
 
-        this.$watch('page', (newPage) => {
-            this.getTickets(newPage)
+        Promise.all([
+            axios.get(window.traq.base + 'api/types'),
+            axios.get(window.traq.base + 'api/statuses'),
+        ]).then(([types, statuses]) => {
+            this.types = types.data
+            this.statuses = statuses.data
+        })
+
+        this.$watch('page', () => {
+            this.getTickets()
+        })
+
+        this.$watch('status', () => {
+            this.getTickets()
+        })
+
+        this.$watch('type', () => {
+            this.getTickets()
         })
     },
 
-    getTickets(page: number) {
+    getTickets() {
         this.isLoading = true
-        return axios.get(window.traq.base + this.projectSlug + '/tickets.json?assigned_to=' + this.userName + '&page=' + page).then(response => {
+
+        let url = window.traq.base + this.projectSlug + '/tickets.json?assigned_to=' + this.userName + '&page=' + this.page + '&order_by=' + this.sortColumn + '.' + this.sortOrder
+
+        if (this.status.length > 0) {
+            url += '&status=' + this.status.join(',')
+        }
+
+        if (this.type.length > 0) {
+            url += '&type=' + this.type.join(',')
+        }
+
+        return axios.get(url).then(response => {
             this.tickets = response.data.tickets
             this.totalPages = response.data.total_pages
             this.page = response.data.page
             this.isLoading = false
         })
+    },
+
+    sortBy(column: string) {
+        if (this.sortColumn === column) {
+            this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc'
+        }
+
+        this.sortColumn = column
+
+        this.getTickets()
     },
 }))
