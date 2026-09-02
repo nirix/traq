e48fb761c49341c5374c57c2fd8a5ee15b664e90
Jack Polgar <hello@jp.id.au>
2026-09-03 00:08:26 +1000

    bump version to 3.8.5

--- vendor/traq/version.php
+++ vendor/traq/version.php
@@ -1,8 +1,8 @@
 <?php
 /*!
  * Traq
- * Copyright (C) 2009-2025 Jack P.
- * Copyright (C) 2012-2025 Traq.io
+ * Copyright (C) 2009-2026 Jack P.
+ * Copyright (C) 2012-2026 Traq.io
  * https://github.com/nirix
  * http://traq.io
  *
@@ -22,14 +22,14 @@
  */
 
 // Version
-define("TRAQ_VER", "3.8.4"); // Pretty obvious...
+define("TRAQ_VER", "3.8.5"); // Pretty obvious...
 
 // Version code
 // 1.2.3   -> 10203
 // 1.3(.0) -> 10300
 // 1.12.1  -> 11201
 // 1.2.11  -> 10211
-define("TRAQ_VER_CODE", 30804); // Used to check for new versions
+define("TRAQ_VER_CODE", 30805); // Used to check for new versions
 define("TRAQ_DB_VER", 30800);   // Database version
 
 // API version
